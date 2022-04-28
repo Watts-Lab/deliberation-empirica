e3346006ce7a781284bf3ade50b2682d261587d0
James Houghton <James.p.houghton@gmail.com>
2022-04-28 14:41:04 -0400

    updates to videocall

--- deliberation/client/src/pages/Discussion.jsx
+++ deliberation/client/src/pages/Discussion.jsx
@@ -1,10 +1,11 @@
-import { usePlayer } from "@empirica/player";
+import { usePlayer, useRound } from "@empirica/player";
 import React from "react";
 import { VideoCall } from "../components/VideoCall";
 import { Topic } from "../components/Topic";
 
 export function Discussion() {
   const player = usePlayer();
+  const round = useRound();
 
   function handleSubmit() {
     player.stage.set("submit", true);
@@ -13,7 +14,7 @@ export function Discussion() {
   return (
     <div className="md:min-w-120 lg:min-w-200 xl:min-w-400 flex flex-col items-center space-y-10">
       <Topic/>
-      <VideoCall playerName={"Test"}/>
+      <VideoCall playerName={"Nickname Goes Here?"} roomName={round.id}/>
     </div>
   );
 }
