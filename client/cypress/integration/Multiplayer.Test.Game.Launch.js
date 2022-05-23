c3d54fcf59cc47a0369c9047c0b78dbc8f90bde7
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-05-23 16:21:05 -0400

    Write test for launching a multiplayer test game
    As of now this leaves a new batch in empirica each time the test is run - probably want to figure out how to delete the batch it creates at the end of the test

--- client/cypress/integration/Multiplayer.Test.Game.Launch.js
+++ client/cypress/integration/Multiplayer.Test.Game.Launch.js
@@ -1,6 +1,22 @@
-// Multiplayer.Test.Game.Launch.js created with Cypress
+// Experiment.Step.Through.js created with Cypress
 //
 // Start writing your Cypress tests below!
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+// Launch a multiplayer test game using cypress in the admin console, or Empirica API
+
+it ("multiplayer test game launch", () => {
+    //admin screen renders properly
+    cy.visit('http://localhost:3000/admin/');
+    cy.contains("Batches");
+    //create new batch
+    cy.get("button").eq(1).click();
+    cy.get('select').select("Two Players");
+    cy.get('form').submit();
+    cy.get('button').eq(2).click();
+    cy.get('button').eq(2).click();
+    //TODO delete the batch after the test
+
+})
