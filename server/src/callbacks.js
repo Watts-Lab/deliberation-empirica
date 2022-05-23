170b648a524a423d754a4b50bf74a16f2c5448f3
kailyl <kliu279@gmail.com>
2022-05-23 17:20:39 -0400

    attempt to fetch file (unsuccessful)

--- server/src/callbacks.js
+++ server/src/callbacks.js
@@ -3,8 +3,26 @@ import { Callbacks } from "@empirica/admin";
 const Empirica = new Callbacks();
 export default Empirica;
 
+const validLink = new RegExp('(https://raw.).*(blob/)(?=.*\d)(?=.*[a-z]).*(.md)')
+
 Empirica.onGameStart(function ({ game }) {
   console.log("game start");
+
+  fetch('https://raw.githubusercontent.com/Watts-Lab/deliberation-topics/7b9fa478b11c7e14b670beb710a2c4cd98b4be1c/topics/example.md') 
+    .then(response => response.text())
+    .then(text => 
+      round.set("topic", text)
+    );
+
+  // if (validLink.test(game.treatment.url)) {
+  //   fetch(game.treatment.url) 
+  //     .then(response => response.text())
+  //     .then(text => 
+  //       round.set("topic", text)
+  //       );
+  // } else {
+  //   console.log('error');
+  // }
 
   const round = game.addRound({
     name: "Discussion",
