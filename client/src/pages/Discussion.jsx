5398632302d37fee045c77888d1f951476af97ed
bsmith6202 <beccas@sas.upenn.edu>
2022-05-25 09:52:41 -0400

    Update Discussion.jsx

--- client/src/pages/Discussion.jsx
+++ client/src/pages/Discussion.jsx
@@ -11,7 +11,7 @@ export default function Discussion() {
     player.stage.set("submit", true);
   }
 
-  //TODO: change hard coded topic to the topic for the round 
+  //TODO: change hard coded topic to the topic for the round using round constnat
   return (
     <div className="md:min-w-120 lg:min-w-200 xl:min-w-400 flex flex-col items-center space-y-5">
       <Topic topic="abortion"/>
