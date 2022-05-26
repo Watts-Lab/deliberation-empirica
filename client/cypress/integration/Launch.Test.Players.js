2b5efd45c923cf524394d6090684a70392b27b2d
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-05-26 19:20:25 -0400

    Randomized accessed players

--- client/cypress/integration/Launch.Test.Players.js
+++ client/cypress/integration/Launch.Test.Players.js
@@ -23,10 +23,13 @@ describe("multiple test players", () => {
     });
 
     it("first then second", () => {
-        const bob = 'http://localhost:3000/?playerKey=bob';
-        const alice = 'http://localhost:3000/?playerKey=alice'
+        const randomPlayerKey1 = Math.floor(Math.random() * 1e13);
+        const randomPlayerKey2 = Math.floor(Math.random() * 1e13);
+        const url1 = `http://localhost:3000/?playerKey=${randomPlayerKey1}`
+        const url2 = `http://localhost:3000/?playerKey=${randomPlayerKey2}`
 
-        cy.visit(bob);
+        cy.visit(`http://localhost:3000/?playerKey=${Math.floor(Math.random() * 1e13)}`);
+        cy.visit(url1);
         //consent
         cy.contains("Do you consent to participate in this experiment?");
         cy.contains("This experiment is part of a scientific project. Your decision to participate in this experiment is entirely voluntary. There are no known or anticipated risks to participating in this experiment. There is no way for us to identify you. The only information we will have, in addition to your responses, is the timestamps of your interactions with our site. The results of our research may be presented at scientific meetings or published in scientific journals. Clicking on the \"I AGREE\" button indicates that you are at least 18 years of age, and agree to participate voluntary.");
@@ -41,8 +44,8 @@ describe("multiple test players", () => {
         cy.contains("Game loading");
         //cy.get('button').contains("New Player").click({force: true});
 
-        const player2 = 'http://localhost:3000/?playerKey=player2'
-        cy.visit(player2);
+        cy.visit(`http://localhost:3000/?playerKey=${Math.floor(Math.random() * 1e13)}`);
+        cy.visit(url2);
         cy.contains("Do you consent to participate in this experiment?");
         cy.contains("This experiment is part of a scientific project. Your decision to participate in this experiment is entirely voluntary. There are no known or anticipated risks to participating in this experiment. There is no way for us to identify you. The only information we will have, in addition to your responses, is the timestamps of your interactions with our site. The results of our research may be presented at scientific meetings or published in scientific journals. Clicking on the \"I AGREE\" button indicates that you are at least 18 years of age, and agree to participate voluntary.");
         cy.get('button').contains('I AGREE').click();
@@ -54,27 +57,22 @@ describe("multiple test players", () => {
         cy.contains("Instruction One");
         cy.get('button').contains("Next").click({force: true});
         cy.contains("Your deliberation topic is:");
-        cy.contains("Join meeting");
-        cy.wait(2000);
-        cy.contains("Bonus");
-        cy.contains("Exit Survey");
-        cy.contains("Please answer the following short survey. You do not have to provide any information you feel uncomfortable with.");
-        cy.contains("Age");
-        cy.contains("Gender");
-        cy.get("input").first().click().type("20");
-        cy.get("input").eq(2).click().type("male");
-        cy.contains("Highest Education");
-        cy.contains("High School").click();
-        cy.contains("US Bachelor's Degree");
-        cy.contains("Master's or higher");
-        cy.contains("Other");
-        cy.contains("How would you describe your strength in the game?");
-        cy.contains("Do you feel the pay was fair?");
-        cy.contains("Feedback, including problems you encountered.");
-        cy.get('textarea').eq(0).click().type("high");
-        cy.get('textarea').eq(1).click().type("yes");
-        cy.get('textarea').eq(2).click().type("NA");
-        cy.contains("Submit").click();
+        // Discussion
+        cy.get('iframe')
+        // cy.contains("Your deliberation topic is:");
+        // cy.contains("Join meeting");
+        cy.wait(6000);
+
+        //Exit Survey
+        cy.contains("On a scale of zero to ten, how likely are you to recommend our product to a friend or colleague?");
+        cy.contains("5").click();
+        cy.contains("What do you miss and what was disappointing in your experience with us?");
+        cy.get('#sq_103i').click().type("NA");
+        cy.get("#sv-nav-complete > div > input").click();
+
+
+        //finished screen
+        cy.contains("Thank you for your feedback.");
         
 
 
