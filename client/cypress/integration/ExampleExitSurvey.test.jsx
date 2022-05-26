faceef6ab35d8ce576739a73ff9087820b52c9a3
Alan-Qiao <alangxqiao@gmail.com>
2022-05-26 22:40:05 +0200

    Fixed Cypress Test
     - loads tajriba.json after game end to look for correct output of survey result

--- client/cypress/integration/ExampleExitSurvey.test.jsx
+++ client/cypress/integration/ExampleExitSurvey.test.jsx
@@ -1,6 +1,9 @@
-const expectedJSON = {};
+const expectedJSON = {
+    "nps_score":8,
+    "passive_experience": "I don't know but I like 8",
+    "disappointed_experience": "Discussion too short",
+};
 
-// In theory this should work, waiting for Cypress set-up to be merged to main before actually testing and verifying that this works
 describe('Test that survey data saves correctly', () => {
     before(() => {
         cy.clearLocalStorage();
@@ -21,13 +24,16 @@ describe('Test that survey data saves correctly', () => {
         cy.visit('http://localhost:3000/?playerKey=1682370811758');
         cy.get('button').contains('AGREE').click();
         cy.get('input').type('abcdefg');
+        cy.get('button').contains('Enter').click();
         cy.get('button').contains('Next').click();
         cy.tick(10000);
         cy.get('[type="radio"]').eq(8).check();
         cy.get('textarea[aria-label="What is the primary reason for your score"]').type("I don't know but I like 8");
         cy.get('textarea[aria-label="What do you miss and what was disappointing in your experience with us?"]').type('Discussion too short');
         cy.get('button').contains('Complete').click();
-        // need to find out where is player.json being stored
-        cy.readFile('figure out filePath for player.json').its('playerId').should('deep.eq', expectedJSON);
+        // temporary solution with testing existence in tajriba.json
+        cy.readFile('../.empirica/tajriba.json').then(data => {
+            expect(Object.values(data).includes(JSON.stringify(expectedJSON))).to.be.true;
+        });
     })
 })
