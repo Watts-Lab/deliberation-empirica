2bbbfa787c75ced3d226cb65c2ae5a24e7efdc14
bsmith6202 <beccas@sas.upenn.edu>
2022-05-26 10:01:50 -0400

    Update Discussion.jsx

--- client/src/pages/Discussion.jsx
+++ client/src/pages/Discussion.jsx
@@ -11,7 +11,7 @@ export default function Discussion(props) {
   }
 
   return (
-    <div className="md:min-w-120 md:min-h-120 lg:min-w-200 xl:min-w-400 flex flex-col items-center top:5px space-y-5">
+    <div className="md:min-w-100 md:min-h-160 lg:min-w-200 xl:min-w-400 flex flex-col items-center top:5px space-y-5">
       <Topic topic={round.topic}/>
       <VideoCall playerName={"Nickname Goes Here?"} roomName={round.id}/>
     </div>
