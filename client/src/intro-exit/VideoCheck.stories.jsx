1b0cd4c6ff70b50223fc01d2080e9ceb26c3eb95
bsmith6202 <beccas@sas.upenn.edu>
2022-06-01 16:47:18 -0400

    Create VideoCheck.stories.jsx
    added a storybook story (set the value of next back to the VideoCheck to simplify imports, since the storybook story was mostly just to check visuals

---
+++ client/src/intro-exit/VideoCheck.stories.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import VideoCheck from './VideoCheck';
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'VideoCheck',
+  component: VideoCheck,
+};
+
+//👇 We create a “template” of how args map to rendering
+const Template = (args) => <VideoCheck {...args} />;
+
+
+//👇 Each story then reuses that template
+export const Primary = Template.bind({});
+Primary.args = {
+   next: VideoCheck
+};
