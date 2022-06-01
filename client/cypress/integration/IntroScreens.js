aab99c48501a7ec42dc61e7b93c1aa3a2666d778
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-06-01 17:38:20 -0400

    Write cypress tests

--- client/cypress/integration/IntroScreens.js
+++ client/cypress/integration/IntroScreens.js
@@ -1,5 +1,3 @@
-import React from "React";
-import CheckUnderstanding from "src/intro-exit/CheckUnderstanding";
 
 
 describe("intro screen tests", () => {
@@ -11,11 +9,56 @@ describe("intro screen tests", () => {
         cy.get('button').contains(" Start").click();
     })
 
+    beforeEach(() => {
+        const randomPlayerKey1 = Math.floor(Math.random() * 1e13);
+        const randomPlayerKey2 = Math.floor(Math.random() * 1e13);
+        cy.visit(`http://localhost:3000/?playerKey=${randomPlayerKey1}`);
+        cy.visit(`http://localhost:3000/?playerKey=${randomPlayerKey2}`);
+        //consent
+        cy.contains("Do you consent to participate in this experiment?");
+        cy.contains("This experiment is part of a scientific project. Your decision to participate in this experiment is entirely voluntary. There are no known or anticipated risks to participating in this experiment. There is no way for us to identify you. The only information we will have, in addition to your responses, is the timestamps of your interactions with our site. The results of our research may be presented at scientific meetings or published in scientific journals. Clicking on the \"I AGREE\" button indicates that you are at least 18 years of age, and agree to participate voluntary.");
+        cy.get('button').contains('I AGREE').click();
+
+        // Login
+        cy.contains("Enter your Player Identifier");
+        cy.get('input').click().type('cy');
+        cy.get('button').contains("Enter").click();
+        cy.wait(100);
+
+        cy.get('button').contains("Next").click();
+    })
+
+    afterEach(() => {
+        cy.contains("Reset Current Session").click({force: true});
+    });
+
     after(() => {
         cy.exec("cd .. && cd .empirica/local && rm tajriba.json", {failOnNonZeroExit: false});
     })
 
     it("fail understanding check", () => {
         cy.contains("Answer the following questions to confirm your understanding of the instructions.");
+        cy.get('input').contains("Partcipate in a discussion with other participants").click();
+        cy.get('input').contains("Write about your group's discussion").click();
+        cy.get('input').contains("To be anonmously published in academic venues.").click();
+        cy.get('input').contains("To disclose to other participants during the session").click();
+        cy.get('input').contains("To analyze for behavioral patterns to support our research.").click();
+        cy.get('input').contains("To share with select researchers under confidentiality agreements.").click();
+        cy.get('input').contains("5-10 minutes").click();
+        cy.wait(200);
+        cy.contains("Answer the following questions to confirm your understanding of the instructions.");
+    });
+
+    it("pass understanding check", () => {
+        cy.contains("Answer the following questions to confirm your understanding of the instructions.");
+        cy.get('input').contains("Partcipate in a discussion with other participants").click();
+        cy.get('input').contains("Write about your group's discussion").click();
+        cy.get('input').contains("To be anonmously published in academic venues.").click();
+        cy.get('input').contains("To disclose to other participants during the session").click();
+        cy.get('input').contains("To analyze for behavioral patterns to support our research.").click();
+        cy.get('input').contains("To share with select researchers under confidentiality agreements.").click();
+        cy.get('input').contains("5-10 minutes").click();
+        cy.wait(200);
+        cy.get('iframe');
     })
 })
