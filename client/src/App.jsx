d0a03e62939a7b7c5bc903c09e91b91276ceb4fb
Skylar Rearick <rearick.skylar@gmail.com>
2022-06-03 10:51:54 -0400

    hard-coded exit surveys

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import { Game } from "./Game";
 import { ExitSurvey } from "./intro-exit/ExitSurvey";
 import ExampleExitSurvey from './intro-exit/Surveys/ExampleExitSurvey';
 import { Introduction } from "./intro-exit/Introduction";
+import teamViability from "./intro-exit/Surveys/teamViability";
+import QCSurvey from "./intro-exit/Surveys/QCSurvey";
 
 export function getURL() {
   const host = window.location.hostname;
@@ -25,7 +27,7 @@ export default function App() {
       <EmpiricaMenu />
       <div className="h-full overflow-auto">
         <EmpiricaPlayer url={getURL()} ns={playerKey}>
-          <GameFrame introSteps={[Introduction]} exitSteps={[ExampleExitSurvey]}>
+          <GameFrame introSteps={[Introduction]} exitSteps={[teamViability, QCSurvey]}>
             <Game />
           </GameFrame>
         </EmpiricaPlayer>
