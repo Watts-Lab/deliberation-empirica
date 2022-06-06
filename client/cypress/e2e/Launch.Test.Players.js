59e933706479014ed34055447aa9ceed91d65d6a
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-06-06 15:41:18 -0400

    Test passes!

--- client/cypress/e2e/Launch.Test.Players.js
+++ client/cypress/e2e/Launch.Test.Players.js
@@ -13,7 +13,7 @@ describe("multiple test players", () => {
         cy.clearLocalStorage();
         cy.visit('http://localhost:3000/admin/');
         cy.get("button").contains('New Batch').click();
-        cy.get('select').select("2 players 60 seconds");
+        cy.get('select').select("2 players 6 seconds");
         cy.get('form').submit();
         cy.get('button').contains(" Start").click();
     })
@@ -36,9 +36,9 @@ describe("multiple test players", () => {
         cy.get('button').contains('I AGREE').click();
         // Login
         cy.contains("Enter your Player Identifier");
-        cy.get('input').click().type('bob');
-        cy.get('button').contains("Enter").click();        
-        // Instruction one
+        cy.get('input').click().type(randomPlayerKey1);
+        cy.get('button').contains("Enter").click();  
+        cy.wait(100);      
         cy.get('button').contains("Next").click({force: true});
         cy.get('input').click().type(randomPlayerKey1);
         cy.get('button').contains("Next").click();  
@@ -50,8 +50,7 @@ describe("multiple test players", () => {
         cy.contains(" I am in a space where I can speak freely without bothering other people.").click();
         cy.contains(" I will not be interrupted").click();
         cy.get('button').contains("Next").click(); 
-        cy.get("button").contains("Next").click();
-        cy.contains("Game loading");
+        
         cy.contains("Answer the following questions to confirm your understanding of the instructions.");
         cy.get('label').contains("Partcipate in a discussion with other participants").click();
         cy.get('label').contains("Write about your group's discussion").click();
@@ -62,6 +61,7 @@ describe("multiple test players", () => {
         cy.get('label').contains("To share with select researchers under confidentiality agreements.").click();
         cy.get('label').contains("15-35 minutes").click();
         cy.get('button').contains("Next").click();
+        cy.contains("Game loading");
         // cy.get('button').contains("New Player").invoke('removeAttr', 'target').click({force: true});
 
         cy.visit(`http://localhost:3000/?playerKey=${Math.floor(Math.random() * 1e13)}`);
@@ -73,11 +73,29 @@ describe("multiple test players", () => {
         cy.contains("Enter your Player Identifier");
         cy.get('input').click().type(randomPlayerKey2);
         cy.get('button').contains("Enter").click();        
-        // Instruction one
+        cy.wait(100);      
         cy.get('button').contains("Next").click({force: true});
-        cy.visit(url1);
-        cy.wait(1000);
-        cy.contains("Your deliberation topic is:");
+        cy.get('input').click().type(randomPlayerKey1);
+        cy.get('button').contains("Next").click();  
+        cy.contains(" My camera and microphone are enabled.").click();
+        cy.contains(" I can see my full face in the video window."). click();
+        cy.contains(" (i.e. a diploma on the wall, the name of an employer).").click();
+        cy.contains(" My background doesn't reveal other personal information I am not comfortable sharing.").click();
+        cy.contains(" I am in a safe place to engage in a discussion.").click();
+        cy.contains(" I am in a space where I can speak freely without bothering other people.").click();
+        cy.contains(" I will not be interrupted").click();
+        cy.get('button').contains("Next").click(); 
+        
+        cy.contains("Answer the following questions to confirm your understanding of the instructions.");
+        cy.get('label').contains("Partcipate in a discussion with other participants").click();
+        cy.get('label').contains("Write about your group's discussion").click();
+        cy.get('label').contains("To be anonmously published in academic venues.").click();
+        cy.get('label').contains("To disclose to other participants during the session").click();
+        cy.get('label').contains("For quality control").click();
+        cy.get('label').contains("To analyze for behavioral patterns to support our research.").click();
+        cy.get('label').contains("To share with select researchers under confidentiality agreements.").click();
+        cy.get('label').contains("15-35 minutes").click();
+        cy.get('button').contains("Next").click();
         // Discussion
         cy.get('iframe')
         // cy.contains("Your deliberation topic is:");
@@ -88,12 +106,12 @@ describe("multiple test players", () => {
         cy.contains("On a scale of zero to ten, how likely are you to recommend our product to a friend or colleague?");
         cy.contains("5").click();
         cy.contains("What do you miss and what was disappointing in your experience with us?");
-        cy.get('#sq_103i').click().type("NA");
-        cy.get("#sv-nav-complete > div > input").click();
+        cy.get('textarea').type("NA");
+        cy.get("input[value='Complete']").click();
 
 
         //finished screen
-        cy.contains("Thank you for your feedback.");
+        cy.contains("Finished");
         
 
 
