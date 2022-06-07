1b182d73970af7782785d58924cf90f304806a03
James Houghton <james.p.houghton@gmail.com>
2022-06-07 13:07:31 -0400

    Cypress runs in root folder

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -4,8 +4,9 @@ module.exports = {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('plugins/index.js')(on, config)
+      return require('../cypress/plugins/index.js')(on, config)
     },
-    specPattern: 'e2e/**/*.{js,jsx,ts,tsx}',
+    specPattern: '../cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    supportFile: '../cypress/support/e2e.{js,jsx,ts,tsx}'
   },
 }
