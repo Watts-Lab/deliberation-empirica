39e17ba58d744d8702cc9d8917ce60161821d54e
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-06-08 15:21:00 -0400

    Use create button instead in exampleexitsurvey

--- cypress/e2e/ExampleExitSurvey.test.jsx
+++ cypress/e2e/ExampleExitSurvey.test.jsx
@@ -9,12 +9,13 @@ describe('Test that survey data saves correctly', () => {
     before(() => {
         cy.clearLocalStorage();
         cy.visit('http://localhost:3000/admin/');
-        cy.get('button').contains('New Batch').click();
-        cy.get('button').contains('Simple').click();
-        cy.get('select').first().select('1 player 6 seconds');
+        cy.get("button").contains('New Batch').click();
+        cy.get('select').select("1 player 6 seconds");
+        //TODO set discussion duration to 1 second in treatment perameters
         cy.contains('game', { timeout: 500 }).should('be.visible');
-        cy.get('form').submit();
-        cy.get('button').contains(' Start').click();
+        cy.get('button').contains("Create").click({force: true})
+        //cy.get('form').submit();
+        cy.get('button').contains(" Start").click();
     })
 
     // after(() => {
