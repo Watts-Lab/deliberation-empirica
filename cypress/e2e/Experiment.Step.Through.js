ab86e7d4ad5d4821c4cf423c2defc88d9ae2f08e
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-06-08 15:02:07 -0400

    timeout for create submission

--- cypress/e2e/Experiment.Step.Through.js
+++ cypress/e2e/Experiment.Step.Through.js
@@ -19,6 +19,7 @@ describe("user perspective", () => {
         cy.get("button").contains('New Batch').click();
         cy.get('select').select("1 player 6 seconds");
         //TODO set discussion duration to 1 second in treatment perameters
+        cy.contains('game', { timeout: 500 }).should('be.visible');
         cy.get('form').submit();
         cy.get('button').contains(" Start").click();
     })
