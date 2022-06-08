f7358dfa3de28f0c9aea692a41872f17e9389dfa
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-06-08 15:33:17 -0400

    remove force

--- cypress/e2e/ExampleExitSurvey.test.jsx
+++ cypress/e2e/ExampleExitSurvey.test.jsx
@@ -13,7 +13,7 @@ describe('Test that survey data saves correctly', () => {
         cy.get('select').select("1 player 6 seconds");
         //TODO set discussion duration to 1 second in treatment perameters
         cy.contains('game', { timeout: 500 }).should('be.visible');
-        cy.get('button').contains("Create").click({force: true})
+        cy.get('button').contains("Create").click()
         //cy.get('form').submit();
         cy.get('button').contains(" Start").click();
     })
