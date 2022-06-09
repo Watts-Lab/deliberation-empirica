bbb737f85ac847340c4f9dad63ef803cbd1cd04c
James Houghton <james.p.houghton@gmail.com>
2022-06-09 16:16:09 -0400

    Update Normal_Paths.js

--- cypress/e2e/Normal_Paths.js
+++ cypress/e2e/Normal_Paths.js
@@ -4,72 +4,122 @@
 
 
 describe("normal_paths", () => {
-  const soloCondition = "cypress1";
+    const condition = "cypress1";
+    // launching 3 players for a 2-slot game means we can test the overflow/sorry page
+    const playerKeys = [
+        'test_'+Math.floor(Math.random() * 1e13),
+        'test_'+Math.floor(Math.random() * 1e13)
+    ]
 
-  before(() => {
-    cy.viewport(2000, 1000)
-    cy.visit('/admin/');
+    before(() => {
+        cy.viewport(2000, 1000)
+        cy.visit('/admin/');
 
-    // wait for page load
-    cy.contains('Batches are groups of Games', { timeout: 5000 } ).should('be.visible');
-    
-    //start all existing unstarted batches
-    cy.get('body')
-      .then( ($body) => {
-          const startButtons = $body.find('button:contains("Start")')
-          cy.log(startButtons.length + " unlaunched batches will be started")
-          if ( startButtons.length ) {
-              cy.wrap(startButtons, {log: false}).each(
+        // wait for page load
+        cy.contains('Batches are groups of Games', { timeout: 5000 } ).should('be.visible');
+        
+        //start all existing unstarted batches
+        cy.get('body')
+        .then( ($body) => {
+            const startButtons = $body.find('button:contains("Start")')
+            cy.log(startButtons.length + " unlaunched batches will be started")
+            if ( startButtons.length ) {
+                cy.wrap(startButtons, {log: false}).each(
+                    ($button, index, $list) => {
+                    cy.wrap($button, {log: false}).click()
+                    } 
+                )
+            } 
+        })
+        cy.waitUntil(() => cy.get('body').then( $body => $body.find('button:contains("Start")').length < 1),
+                    {customMessage:"all unlaunched games are started"})
+        
+        //stop all existing unstarted batches
+        cy.get('body')
+        .then( ($body) => {
+            const stopButtons = $body.find('button:contains("Stop")')
+            cy.log(stopButtons.length + " running games will be stopped")
+            if ( stopButtons.length ) {
+                cy.wrap(stopButtons, {log: false}).each(
                 ($button, index, $list) => {
-                  cy.wrap($button, {log: false}).click()
+                    cy.wrap($button, {log: false}).click()
                 } 
-              )
-          } 
-    })
-    cy.waitUntil(() => cy.get('body').then( $body => $body.find('button:contains("Start")').length < 1),
-                 {customMessage:"all unlaunched games are started"})
-    
-    //stop all existing unstarted batches
-    cy.get('body')
-    .then( ($body) => {
-        const stopButtons = $body.find('button:contains("Stop")')
-        cy.log(stopButtons.length + " running games will be stopped")
-        if ( stopButtons.length ) {
-            cy.wrap(stopButtons, {log: false}).each(
-              ($button, index, $list) => {
-                cy.wrap($button, {log: false}).click()
-              } 
-            )
-        } 
-    })
-    cy.waitUntil(() => cy.get('body').then( $body => $body.find('button:contains("Stop")').length < 1),
-                 {customMessage:"all games are stopped"}
-    )
+                )
+            } 
+        })
+        cy.waitUntil(() => cy.get('body').then( $body => $body.find('button:contains("Stop")').length < 1),
+                    {customMessage:"all games are stopped"}
+        )
 
-    //enter new batch drawer
-    cy.get('button').contains('New Batch').click()
-    cy.contains('Create a new Batch with Simple', { timeout: 500 } ).should('be.visible');
-    cy.get('select').select(soloCondition);
-    cy.contains('game', { timeout: 500 }).should('be.visible'); // wait for the condition to be loaded
-    cy.get('form').submit();
+        //enter new batch drawer
+        cy.get('button').contains('New Batch').click()
+        cy.contains('Create a new Batch with Simple', { timeout: 500 } ).should('be.visible');
+        cy.get('select').select(condition);
+        cy.contains('game', { timeout: 500 }).should('be.visible'); // wait for the condition to be loaded
+        cy.get('form').submit();
 
-    //return from new batch drawer
-    cy.contains('Create a new Batch with Simple', { timeout: 500 } ).should('not.exist');
-    cy.get('tr').last().should(($tr) => {
-        expect($tr).to.contain("Created")
-        expect($tr).to.contain(soloCondition)
+        //return from new batch drawer
+        cy.contains('Create a new Batch with Simple', { timeout: 500 } ).should('not.exist');
+        cy.get('tr').last().should(($tr) => {
+            expect($tr).to.contain("Created")
+            expect($tr).to.contain(condition)
+        });
+        cy.contains("Created", { timeout: 500 } );
+        
     });
-    cy.contains("Created", { timeout: 500 } );
-    
-  });
 
-  it("passes", () => {
-    const randomPlayerKey1 = Math.floor(Math.random() * 1e13);
-    const randomPlayerKey2 = Math.floor(Math.random() * 1e13);
-    
-    cy.visit(`?playerKey=${randomPlayerKey1}`)
+    it("walks properly", () => {
+        cy.log("Log in all players")
+        cy.wrap(playerKeys, {log: false}).each( (playerKey) => {
+            cy.visit(`http://localhost:3000/?playerKey=${playerKey}`);
 
-  })
+            cy.log("Consent")
+            cy.contains("Informed Consent", { timeout: 1000 });       
+            cy.get('button').contains('I AGREE').click();
+
+            // Login
+            cy.log("Consent")
+            cy.contains("Enter your", { timeout: 1000 });
+            cy.get('input').click().type(playerKey);
+            cy.get('button').contains("Enter").click();
+        })
+    
+        cy.log("Advance all players to lobby")
+        cy.wrap(playerKeys, {log: false}).each( (playerKey) => {
+            cy.visit(`http://localhost:3000/?playerKey=${playerKey}`);
+
+            //Instructions 
+            cy.contains("About this study:"); 
+            cy.get('button').contains("Next").click({force: true});
+            
+            // Name Input
+            cy.contains("please enter your first name")
+            cy.get('input').click().type(playerKey+'_name');
+            cy.get('button').contains("Next").click();  
+
+            // Video check
+            cy.get('input[id="invisible-button"').click({force: true});
+
+            // Understanding check
+            cy.contains("Answer the following questions to confirm your understanding of the instructions.");
+            cy.get('label').contains("Partcipate in a discussion with other participants").click();
+            cy.get('label').contains("Write about your group's discussion").click();
+            cy.get('label').contains("To be anonmously published in academic venues.").click();
+            cy.get('label').contains("To disclose to other participants during the session").click();
+            cy.get('label').contains("For quality control").click();
+            cy.get('label').contains("To analyze for behavioral patterns to support our research.").click();
+            cy.get('label').contains("To share with select researchers under confidentiality agreements.").click();
+            cy.get('label').contains("15-35 minutes").click();
+            cy.get('button').contains("Next").click();
+        })
+
+        cy.log("Advance first player into game")
+        const playerKey = playerKeys[0]
+
+        cy.visit(`http://localhost:3000/?playerKey=${playerKey}`);
+        cy.wait(10000)
+
+    })
 
 
 })
