192a313e3acdf1ce592303047bd3e36929cbaaed
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-06-13 14:56:38 -0400

    Fix treatments

--- client/src/pages/Discussion.jsx
+++ client/src/pages/Discussion.jsx
@@ -12,7 +12,7 @@ export default function Discussion(props) {
   const {player, round} = props;
   const style = {display: "none"}
   const game = useGame();
-  console.log(game.treatments)
+  console.log(game.treatment)
 
   return (
     <div className="md:min-w-100 md:min-h-160 lg:min-w-200 xl:min-w-400 flex flex-col items-center top:5px space-y-5">
