0409a5a93150b5c75b44f832d03a8da47aa97ddb
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-06-17 09:59:33 -0400

    update checkbox logic in videocheck

--- client/src/intro-exit/VideoCheck.jsx
+++ client/src/intro-exit/VideoCheck.jsx
@@ -23,16 +23,16 @@ export default function VideoCheck({next, usePlayer}) {
     const [enabled, setEnabled] = React.useState(false);
     const [iframeEnabled, setIframeEnabled] = React.useState(window.Cypress ? false : true); //default hide in cypress test
 
-    const Checkbox = ({ label, value, onChange, }) => {
-        return (
-          <div>
-            <label>
-                <input type="checkbox" checked={value} onChange={onChange}/>
-                {label}
-            </label>
-          </div>
-        );
-      };
+    // const Checkbox = ({ label, value, onChange, }) => {
+    //     return (
+    //       <div>
+    //         <label>
+    //             <input type="checkbox" checked={value} onChange={onChange}/>
+    //             {label}
+    //         </label>
+    //       </div>
+    //     );
+    //   };
 
     function handleSubmit(event) {
         if (canSee && 
@@ -78,7 +78,49 @@ export default function VideoCheck({next, usePlayer}) {
             <div className="mt-4">
                 <form style={questionsStyle} onSubmit={handleSubmit}>
                     <div className="ml-5 space-y-1">
-                        <Checkbox
+                        <div className="mt-1">
+                            <input className="mr-2" type="checkbox" id="enabled" onClick={(cb) => setEnabled(cb.checked)}/>
+                            <label htmlFor="enabled" className="text-sm font-medium text-gray-700 my-2">
+                                My camera and microphone are enabled.
+                            </label>
+                        </div>
+                        <div className="mt-1">
+                            <input className="mr-2" type="checkbox" id="see" onClick={(cb) => setSee(cb.checked)}/>
+                            <label htmlFor="see" className="text-sm font-medium text-gray-700 my-2">
+                                I can see my full face in the video window.
+                            </label>
+                        </div>
+                    <div className="mt-1">
+                        <input className="mr-2" type="checkbox" id="noName" onClick={(cb) => setNoName(cb.checked)}/>
+                        <label htmlFor="noName" className="text-sm font-medium text-gray-700 my-2">
+                            Nothing in my background reveals my full name (i.e. a diploma on the wall, the name of an employer).
+                        </label>
+                    </div>
+                    <div className="mt-1">
+                        <input className="mr-2" type="checkbox" id="background" onClick={(cb) => setBackground(cb.checked)}/> 
+                        <label htmlFor="background" className="text-sm font-medium text-gray-700 my-2">
+                            My background doesn't reveal other personal information I am not comfortable sharing.
+                        </label> 
+                    </div>
+                    <div className="mt-1">
+                        <input className="mr-2" type="checkbox" id="safeplace" onClick={(cb) => setSafePlace(cb.checked)}/>    
+                        <label htmlFor="safeplace" className="text-sm font-medium text-gray-700 my-2">
+                            I am in a safe place to engage in a discussion.
+                        </label> 
+                    </div>
+                    <div className="mt-1">
+                        <input className="mr-2" type="checkbox" id="speakFree" onClick={(cb) => setSpeakFree(cb.checked)}/>  
+                        <label htmlFor="speakfree" className="text-sm font-medium text-gray-700 my-2">
+                            I am in a space where I can speak freely without bothering other people.
+                        </label>
+                    </div>
+                    <div className="mt-1">                   
+                        <input className="mr-2" type="checkbox" id="noInterrupt" onClick={(cb) => setNoInterrupt(cb.checked)}/> 
+                        <label htmlFor="noInterrupt" className="text-sm font-medium text-gray-700 my-2">
+                            I will not be interrupted.
+                        </label>    
+                    </div> 
+                        {/* <Checkbox
                             label=" My camera and microphone are enabled."
                             value={enabled}
                             onChange={(e) => setEnabled(!enabled)}
@@ -112,7 +154,7 @@ export default function VideoCheck({next, usePlayer}) {
                             label=" I will not be interrupted."
                             value={noInterrupt}
                             onChange={(e) => setNoInterrupt(!noInterrupt)}
-                        />
+                        /> */}
                     </div>
 
                     <Button type="submit" base='inline-flex items-center px-4 py-2 mt-6 border text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-empirica-500'>
