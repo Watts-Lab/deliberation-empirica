4a39fa9cdaaf001157e8a5b9eb5e7df7d931d4d8
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-06-22 14:59:24 -0400

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,35 +15,19 @@ import { Button } from "./components/Button";
 Sentry.init({
   dsn: "https://bbe62f66328d40c6bf9008b293e44d7d@o1288526.ingest.sentry.io/6505477",
   integrations: [new BrowserTracing()],
-  beforeSend(event, hint) {
-    Sentry.showReportDialog({ 
-      eventId: event.event_id
-    });
-  },
+  // beforeSend(event, hint) {
+  //   Sentry.showReportDialog({ 
+  //     eventId: event.event_id
+  //   });
+  // },
   attachStacktrace: true,
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
-  enabled: process.env.NODE_ENV !== 'development'
+  //enabled: process.env.NODE_ENV !== 'development'
 });
-
-function handleProblem() {
-  Sentry.showReportDialog({
-    eventId: Sentry.captureEvent({
-      message: 'User Feedback',
-      stacktrace: [
-        Sentry.captureMessage
-      ],
-    }),
-    subtitle: "Please let us know what went wrong below.",
-    subtitle2: '',
-    labelComments: 'Please describe what happened'
-  })
-}
-
-
 
 
 
@@ -61,13 +45,20 @@ ReactDOM.render(
             top: 5,
           }}
           base='inline-flex items-center px-4 py-2 mt-6 border text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-empirica-500'
-          handleClick={() =>
-            Sentry.showReportDialog({ 
-              eventId: '9fac2ceed9344f2bbfdd1fdacb0ed9b1',
+          handleClick={() => {
+            Sentry.showReportDialog({
+              eventId: Sentry.captureEvent({
+                message: 'User Feedback',
+                stacktrace: [
+                  Sentry.captureMessage
+                ],
+              }),
               subtitle: "Please let us know what went wrong below.",
               subtitle2: '',
+              labelName: "MTurk ID",
+              labelEmail: "What stage in the experiment were you in?",
               labelComments: 'Please describe what happened'
-            })
+            })}
           }
         >
             Report feedback
