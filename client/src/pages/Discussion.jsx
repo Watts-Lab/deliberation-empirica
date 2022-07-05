a0696c68709f91f21bab2924ac2c151f9a4df4e2
Skylar Rearick <rearick.skylar@gmail.com>
2022-07-05 10:36:38 -0400

    tweaking discussion survey

--- client/src/pages/Discussion.jsx
+++ client/src/pages/Discussion.jsx
@@ -3,35 +3,43 @@ import { VideoCall } from "../components/VideoCall";
 import Topic from "../components/Topic";
 import { useState } from "react";
 import { useGame } from "@empirica/player";
+import DiscussionSurvey from "../intro-exit/Surveys/gov_reduce_income_inequality";
 
 export default function Discussion(props) {
   const {player, round} = props;
   const invisibleStyle = {display: "none"};  
   const game = useGame();
-  console.log(game.treatment)
+  console.log(game.treatment);
 
   const [iframeEnabled, setIframeEnabled] = React.useState(window.Cypress ? false : true); //default hide in cypress test
 
   return (
-    <div className="md:min-w-100 md:min-h-160 lg:min-w-200 xl:min-w-400 flex flex-col items-center top:5px space-y-5">
-      <Topic topic={round.get("topic")}/>
+    <div className="flex flex-col items-center top:5px space-y-5">
+      <div>
+        <Topic topic={round.get("topic")}/>
 
-      <input type="checkbox" data-test="enableIframe" id="enableIframeCB" onClick={(cb)=>setIframeEnabled(cb.checked)} style={invisibleStyle}></input>
-      <input type="submit" data-test="skip" style={invisibleStyle} onClick={() => player.stage.set("submit", true)}></input>
+        <input type="checkbox" data-test="enableIframe" id="enableIframeCB" onClick={(cb)=>setIframeEnabled(cb.checked)} style={invisibleStyle}></input>
+        <input type="submit" data-test="skip" style={invisibleStyle} onClick={() => player.stage.set("submit", true)}></input>
+      </div>
 
-      {iframeEnabled && <VideoCall 
-        playerName={player.get("name")}
-        roomName={round.id} 
-        position={'absolute'} 
-        left={'0%'} 
-        right ={'5%'}
-        height = {'100%'}
-        width = {'100%'} 
-        disableRemoteVideoMenu = {game.treatment.disableRemoteVideoMenu}
-        disableRemoteMute = {game.treatment.disableRemoteMute}
-        disableKick = {game.treatment.disableKick}
-      />
+      {iframeEnabled && 
+        <div className="grid grid-col-3">
+          <div className="col-span-2">
+            <VideoCall 
+              playerName={player.get("name")}
+              roomName={round.id}  
+              disableRemoteVideoMenu = {game.treatment.disableRemoteVideoMenu}
+              disableRemoteMute = {game.treatment.disableRemoteMute}
+              disableKick = {game.treatment.disableKick}
+            />
+          
+          </div>
+          <div>
+            <DiscussionSurvey className="max-w-md"/>
+          </div>
+        </div>
       }
+
     </div>
   );
 }
