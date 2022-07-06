9842122f5b6887c6168f61b4dcbe0a37a26a2beb
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-07-06 13:06:47 -0400

    set discussion response in player and stage on click

--- client/src/components/Topic.jsx
+++ client/src/components/Topic.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown'
+import { usePlayer } from "@empirica/player";
+import { useStage } from "@empirica/player";
 
 export default function Topic(props) {
     const stringTopic = JSON.stringify(props.topic)
@@ -11,21 +13,29 @@ export default function Topic(props) {
         answers[i] = answers[i].replace("\\n", "");
         answers[i] = answers[i].replace('"', "")
       }
+    
+    const player = usePlayer();
+    const stage = useStage();
+    
+    function handleSelect() {
+        player.set("discussionResponse", document.querySelector('input[name="response"]:checked').id);
+        stage.set("discussionResponse", document.querySelector('input[name="response"]:checked').id);
+    }
 
 
 
     return (
         <form>
             <ReactMarkdown>{question}</ReactMarkdown>
-            <input type="radio" id={answers[1]} name="response"></input> 
-            <label htmlFor={answers[1]}> {answers[1]}</label><br></br>
-            <input type="radio" id={answers[2]} name="response"></input>
+            <input type="radio" id={answers[1]} name="response" onClick={handleSelect}></input> 
+            <label htmlFor={answers[1]}> {answers[1]} </label><br></br>
+            <input type="radio" id={answers[2]} name="response" onClick={handleSelect}></input>
             <label htmlFor={answers[2]}> {answers[2]}</label><br></br>
-            <input type="radio" id={answers[3]} name="response"></input>
+            <input type="radio" id={answers[3]} name="response" onClick={handleSelect}></input>
             <label htmlFor={answers[3]}> {answers[3]}</label><br></br>
-            <input type="radio" id={answers[4]} name="response"></input>
+            <input type="radio" id={answers[4]} name="response" onClick={handleSelect}></input>
             <label htmlFor={answers[4]}> {answers[4]}</label><br></br>
-            <input type="radio" id={answers[5]} name="response"></input>
+            <input type="radio" id={answers[5]} name="response" onClick={handleSelect}></input>
             <label htmlFor={answers[5]}> {answers[5]}</label><br></br>
         </form>
         
