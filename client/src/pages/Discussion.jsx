cab095b9525583911d76494e5045beda676dc51d
James Houghton <james.p.houghton@gmail.com>
2022-07-11 09:51:45 -0400

    reformat topic

--- client/src/pages/Discussion.jsx
+++ client/src/pages/Discussion.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { VideoCall } from "../components/VideoCall";
 import Topic from "../components/Topic";
 import { useState } from "react";
-import { useGame, usePlayer, useRound } from "@empirica/player";
+import { useGame, usePlayer, useRound, useStage } from "@empirica/player";
 
 export default function Discussion(props) {
   const player = usePlayer();
   const round = useRound();
+  const stage = useStage();
   const invisibleStyle = {display: "none"};  
   const game = useGame();
   console.log(game.treatment)
@@ -16,7 +17,7 @@ export default function Discussion(props) {
   return (
     <div className="md:min-w-100 md:min-h-160 lg:min-w-200 xl:min-w-400 flex flex-col items-center top:5px space-y-5">
       <h2>Plesae answer the following survey question as a group:</h2>
-      <Topic topic={round.get("topic")}/>
+      <Topic topic={round.get("topic")} responseOwner={stage} submitButton={false}/>
 
       <input type="checkbox" data-test="enableIframe" id="enableIframeCB" onClick={(cb)=>setIframeEnabled(cb.checked)} style={invisibleStyle}></input>
       <input type="submit" data-test="skip" style={invisibleStyle} onClick={() => player.stage.set("submit", true)}></input>
