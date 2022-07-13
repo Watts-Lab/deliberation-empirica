72342c7e1b392cc7e78ddfa6bf4d0b88aabfe446
James Houghton <james.p.houghton@gmail.com>
2022-07-12 22:37:24 -0400

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "Starting cron for data extraction"
-# give cron access to the environemnt variables
-env >> /etc/environment  
-# start cron daemon
-/etc/init.d/cron start
 # move cron file to the correct folder
 # this cron job pushes data to the datastore repo every 15 mins
 # ref: https://blog.thesparktree.com/cron-in-docker
 mv /scripts/cron_push /etc/cron.d/
+echo "Start of data upload log" > /scripts/log/data_push.log
+# give cron access to the environemnt variables
+env >> /etc/environment  
+# start cron daemon
+/etc/init.d/cron start
 
 echo "Starting empirica"
 empirica serve /app/deliberation.tar.zst
