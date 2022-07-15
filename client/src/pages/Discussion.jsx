485027c8a0c16307827d7cb02b6cfcde44706a3f
bsmith6202 <beccas@sas.upenn.edu>
2022-07-15 09:20:15 -0400

    Update Discussion.jsx

--- client/src/pages/Discussion.jsx
+++ client/src/pages/Discussion.jsx
@@ -3,6 +3,7 @@ import { VideoCall } from "../components/VideoCall";
 import Topic from "../components/Topic";
 import { useState } from "react";
 import { useGame, usePlayer, useRound, useStage } from "@empirica/player";
+import { useEffect } from "react";
 
 export default function Discussion(props) {
   const player = usePlayer();
@@ -10,18 +11,64 @@ export default function Discussion(props) {
   const stage = useStage();
   const invisibleStyle = {display: "none"};  
   const game = useGame();
+
+  const [hasClicked, setHasClicked] = useState(false);
+  //setClicked("true");
+
+  const response = stage.get("topicResponse")
+
   console.log(game.treatment)
+
+  useEffect(() => {
+    //alert("ur answers been changed lol");
+    setHasClicked(true)
+  }, [response]);
+
+  let handleButtonClick = (cb) => {
+    //alert("Some of your answers were incorrect")
+    setIframeEnabled(cb.checked);
+    
+    
+  }
+
+  let handleClick = (event) => {
+    player.stage.set("submit", true)
+    alert("woop")
+  }
+
+  // function word() {
+  //   if (hasClicked == true) {
+  //     return "someone clicked sir"
+  //   } else {
+  //     return "hehe"
+  //   }
+  // }
+  
+  setTimeout(() => {
+    const hiding = document.getElementById('hiding');
+  
+    // 👇️ removes element from DOM
+   hiding.style.display = 'none';
+   setHasClicked(false);
+  
+    // 👇️ hides element (still takes up space on page)
+    // box.style.visibility = 'hidden';
+  }, 10000); // 👈️ time in milliseconds
+  
 
   const [iframeEnabled, setIframeEnabled] = React.useState(window.Cypress ? false : true); //default hide in cypress test
 
   return (
     <div className="md:min-w-100 md:min-h-160 lg:min-w-200 xl:min-w-400 flex flex-col items-center top:5px space-y-5">
       <h2 className="text-lg leading-6 font-medium text-gray-900">Please answer the following survey question as a group:</h2>
-      <Topic topic={round.get("topic")} responseOwner={stage} submitButton={false}/>
+      
+      
+      <Topic topic={round.get("topic")} responseOwner={stage} submitButton={false} onChange={handleClick}/>
 
-      <input type="checkbox" data-test="enableIframe" id="enableIframeCB" onClick={(cb)=>setIframeEnabled(cb.checked)} style={invisibleStyle}></input>
+      <input type="checkbox" data-test="enableIframe" id="enableIframeCB" onClick={handleButtonClick} style={invisibleStyle}></input>
+      {/* submit button just for testing */}
+      {hasClicked && <h3 id="hiding" className="text-sm text-gray-500">Someone changed the selected answer</h3>}
       <input type="submit" data-test="skip" style={invisibleStyle} onClick={() => player.stage.set("submit", true)}></input>
-
       {iframeEnabled && <VideoCall 
         playerName={player.get("name")}
         roomName={round.id} 
