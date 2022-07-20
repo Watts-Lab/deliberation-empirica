6278a2cdbe469ed9ed2cfcae60542d7b3e301443
bsmith6202 <beccas@sas.upenn.edu>
2022-07-20 11:25:48 -0400

    Update Discussion.jsx
    changed name to nickname

--- client/src/pages/Discussion.jsx
+++ client/src/pages/Discussion.jsx
@@ -93,7 +93,7 @@ export default function Discussion(props) {
         <div style={rStyle}>
           <h2 className="text-lg leading-6 font-medium text-gray-900">Please answer the following survey question as a group. </h2>
           <h2 className="text-lg leading-6 font-medium text-gray-900">This is a shared question and the selected answer will update when anyone clicks. </h2>
-          <Topic topic={round.get("topic")} responseOwner={stage} submitButton={false} onChange={handleClick} whoClicked={player.get("name")}/>
+          <Topic topic={round.get("topic")} responseOwner={stage} submitButton={false} onChange={handleClick} whoClicked={player.get("nickname")}/>
           <input type="checkbox" data-test="enableIframe" id="enableIframeCB" onClick={(cb)=>setIframeEnabled(cb.checked)} style={invisibleStyle}></input>
           <input type="submit" data-test="skip" style={invisibleStyle} onClick={() => player.stage.set("submit", true)}></input>
         </div>
