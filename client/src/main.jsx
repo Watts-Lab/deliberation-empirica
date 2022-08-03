95cc0bd0e4c335a180fa8d175338bc9bd32f59af
Alan Qiao <33496657+Alan-Qiao@users.noreply.github.com>
2022-08-03 19:11:18 -0400

    Add error to test script

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,9 @@ import App, { getURL } from './App';
 import './index.css';
 import { Button } from './components/Button';
 
+//this is a purposeful lint error
+var broken = 3
+
 Sentry.init({
   dsn: 'https://bbe62f66328d40c6bf9008b293e44d7d@o1288526.ingest.sentry.io/6505477',
   integrations: [new BrowserTracing()],
