095e18b5e21cf4e0d29a4955e4161caa05e64b3e
Alan-Qiao <alangxqiao@gmail.com>
2022-10-09 05:30:32 -0400

    [WIP] Implement HairCheck
    Not fully tested!!!
    - implemented haircheck component + style based on Daily Prebuilt HairCheck
    - not sure if roomUrl actually exists since this video check happens in intro (before game started)
    - not sure what's the best way to update local stream in preview on device change
    - do I need to manually check that I'm not pushing mic track into localStream?
    - can I just mute this thing?

---
+++ client/src/components/HairCheck.jsx
@@ -0,0 +1,135 @@
+import { usePlayer } from '@empirica/core/player/classic/react';
+import DailyIframe from '@daily-co/daily-js';
+import React, { useEffect, useState } from 'react';
+import { Video } from './Video';
+import './VideoCall.css';
+
+export function HairCheck({ roomUrl }) {
+  const player = usePlayer();
+
+  const dailyObject = DailyIframe.createCallObject();
+  const [localStream, setLocalStream] = useState(null);
+  const [cameras, setCameras] = useState([]);
+  const [microphones, setMicrophones] = useState([]);
+  const [speakers, setSpeakers] = useState([]);
+
+  const refreshDeviceList = async () => {
+    const { devices } = await dailyObject.enumerateDevices();
+    setCameras(devices.filter(d => d.kind === 'videoinput' && d.deviceId !== ''));
+    setMicrophones(devices.filter(d => d.kind === 'audioinput' && d.deviceId !== ''));
+    setSpeakers(devices.filter(d => d.kind === 'audiooutput' && d.deviceId !== ''));
+  }
+
+  const mountListeners = () => {
+    dailyObject.on('available-devices-updated', refreshDeviceList);
+
+    dailyObject.on('selected-devices-updated', event => {
+      const { camera, mic, speaker } = event.devices;
+      if (camera && camera.deviceId !== player.get('camera')) {
+        player.set('camera', camera.deviceId);
+      } else if (mic && mic.deviceId !== player.get('mic')) {
+        player.set('mic', mic.deviceId);
+      } else if (speaker && speaker.deviceId !== player.get('speaker')) {
+        player.set('speaker', speaker.deviceId);
+      }
+    });
+
+    // do I need to rebind tracks when device switches?
+    dailyObject.on('track-started', event => {
+      if (event.participant.local) {
+        localStream.addTrack(event.track);
+      }
+    });
+
+    dailyObject.on('track-stopped', event => {
+      if (event.participant.local) {
+        localStream.removeTrack(event.track);
+      }
+    })
+  };
+
+  useEffect(() => {
+    const connect = async () => {
+      const deviceInfo = await dailyObject.startCamera({url: roomUrl});
+      console.log(deviceInfo);
+      const localParticipant = dailyObject.participants().local;
+      const videoTrack = localParticipant.tracks.audio.persistentTrack;
+      const audioTrack = localParticipant.tracks.video.persistentTrack;
+      setLocalStream(MediaStream([videoTrack, audioTrack]));
+      const { camera, mic, speaker } = await dailyObject.getInputDevices();
+      player.set('camera', camera.deviceId);
+      player.set('mic', mic.deviceId);
+      player.set('speaker', speaker.deviceId);
+      await refreshDeviceList();
+      mountListeners();
+    };
+
+    connect();
+
+    return () => {
+      dailyObject.destroy();
+      const tracks = localStream.getTracks();
+      tracks.forEach(track => track.stop());
+      setLocalStream(null);
+    }
+  }, []);
+
+  const updateCamera = e => {
+    dailyObject.setInputDevicesAsync({ videoDeviceId: e.target.value });
+    player.set('camera', e.target.value);
+  }
+
+  const updateMicrophone = e => {
+    dailyObject.setInputDevicesAsync({ audioDeviceId: e.target.value });
+    player.set('mic', e.target.value);
+  }
+
+  const updateSpeaker = e => {
+    dailyObject.setInputDevicesAsync({ audioDeviceId: e.target.value });
+    player.set('speaker', e.target.value);
+  }
+
+  return (
+    <form className="hair-check">
+      <h1>Choose your hardware</h1>
+      {/* Video preview */}
+      {localStream && <Video className="videoPreview" stream={localStream} />}
+
+      {/* Camera select */}
+      <div>
+        <label htmlFor="cameraOptions">Camera:</label>
+        <select name="cameraOptions" id="cameraSelect" onChange={updateCamera}>
+          {cameras?.map((camera) => (
+            <option key={`cam-${camera.deviceId}`} value={camera.deviceId}>
+              {camera.label}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Microphone select */}
+      <div>
+        <label htmlFor="micOptions">Microphone:</label>
+        <select name="micOptions" id="micSelect" onChange={updateMicrophone}>
+        {microphones?.map((mic) => (
+          <option key={`mic-${mic.deviceId}`} value={mic.deviceId}>
+            {mic.label}
+          </option>
+        ))}
+        </select>
+      </div>
+
+      {/* Speakers select */}
+      <div>
+        <label htmlFor="speakersOptions">Speakers:</label>
+        <select name="speakersOptions" id="speakersSelect" onChange={updateSpeaker}>
+        {speakers?.map((speaker) => (
+          <option key={`speaker-${speaker.deviceId}`} value={speaker.deviceId}>
+            {speaker.label}
+          </option>
+        ))}
+        </select>
+      </div>
+    </form>
+  );
+}
