eac8135d4b6f556413cf129da2b2b13701ae378f
James Houghton <james.p.houghton@gmail.com>
2023-02-06 11:22:50 -0500

    windi issues?

--- client/src/elements/Separator.jsx
+++ client/src/elements/Separator.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-export function Separator({ 
-    style = "", 
-    testId = "unnamedSeparator" 
-}) {
-    
-    return(
-        <div data-test={testId}>
-           {style === "thin" && <hr className="h-px my-4 w-full bg-gray-200"/>}
-           {(style === "" || style === "regular") && <hr className="h-0.5 my-4 w-full bg-gray-200"/>}
-           {style === "thick" && <hr className="h-1 my-4 w-full bg-gray-200"/>}
-        </div>
-    );
+export function Separator({ style = "", testId = "unnamedSeparator" }) {
+  return (
+    <div data-test={testId}>
+      {style === "thin" && <hr className="h-1px my-4 w-full bg-gray-400" />}
+      {(style === "" || style === "regular") && (
+        <hr className="h-3px my-4 w-full bg-gray-400" />
+      )}
+      {style === "thick" && <hr className="h-5px my-4 w-full bg-gray-500" />}
+    </div>
+  );
 }
