f32eb9b1eed332ee054974a8e1e616d63834b2b3
James Houghton <james.p.houghton@gmail.com>
2023-03-20 06:30:52 -0400

    Update Debrief.jsx

--- client/src/intro-exit/Debrief.jsx
+++ client/src/intro-exit/Debrief.jsx
@@ -11,26 +11,24 @@ import { Markdown } from "../components/Markdown";
 const debriefStatements = `
 # Thank you for participating! 🙂
 
-**We will use the completion code you submitted earlier to
-pay your bonus for the rest of the experiment - you don't need to do
-anything else.** 
+Please enter completion code **CptTurk1NDSO32**
 
 ### About this study
-Social scientists have tried many things to improve small group conversations, such as 
-having a facilitator, setting a clear agenda, or establishing ground rules for the discussion. 
+_Social scientists have tried many things to improve small group conversations, such as 
+facilitation, agenda setting, or establishing ground rules. 
 Some of these methods work better than others, but its hard to know which ones will be the most effective. 
-One thing we know for certain is that different types of discussions benefit from different types of support.
+One thing we know for certain is that different types of discussions benefit from different types of support._
 
-Rather than trying to find one trick that will improve all group conversations, our research
+_Rather than trying to find one trick that will improve all group conversations, our research
 team is trying to create a map of what is helpful for each specific type of conversation. 
 Your participation in this study provides data that we will combine with data from many other 
-discussions to understand how a discussion's context shapes its outcomes.
+discussions to understand how a discussion's context shapes its outcomes._
 
-Down the road, this research will help us support groups like parent-teacher organizations or volunteer groups
-in their day-to-day decision making, and will support work to promote civility and understanding between groups.
+_This research will help us support groups like parent-teacher organizations or volunteer groups
+in their day-to-day decision making, and will support work to promote civility and understanding between groups._
 
-For any additional questions, please contact the University of Pennsylvania research team by 
-emailing **[deliberation-study@wharton.upenn.edu](mailto:deliberation-study@wharton.upenn.edu)**.
+_For any additional questions, please contact the University of Pennsylvania research team by 
+emailing **[deliberation-study@wharton.upenn.edu](mailto:deliberation-study@wharton.upenn.edu)**._
 `;
 
 export function Debrief() {
