ac3c96a09e598e65e0931d3b295df614ea6fce2f
James Houghton <james.p.houghton@gmail.com>
2023-04-14 10:44:04 -0500

    update terraform config

--- ecs_service.tf
+++ ecs_service.tf
@@ -1,16 +1,20 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  profile = "aws-csslab-deliberation-seas-acct-PennAccountAdministrator"
 }
 
 resource "aws_security_group" "deliberation" {
   name_prefix = "deliberation"
+  vpc_id      = aws_vpc.deliberation.id
 }
 
 resource "aws_vpc" "deliberation" {
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "deliberation" {
-    vpc_id = aws_vpc.deliberation.id
+  vpc_id = aws_vpc.deliberation.id
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_efs_file_system" "efs" {
@@ -18,7 +22,7 @@ resource "aws_efs_file_system" "efs" {
 }
 
 resource "aws_efs_access_point" "efs" {
-    file_system_id = aws_efs_file_system.efs.id
+  file_system_id = aws_efs_file_system.efs.id
 }
 
 resource "aws_efs_mount_target" "mount_target" {
@@ -27,55 +31,55 @@ resource "aws_efs_mount_target" "mount_target" {
 }
 
 resource "aws_cloudwatch_log_group" "deliberation-empirica-log-group" {
-    name = "/ecs/deliberation-empirica-logs"
+  name = "/ecs/deliberation-empirica-logs"
 }
 
 resource "aws_cloudwatch_log_stream" "deliberation-empirica-log-stream" {
-    name = "deliberation-empirica-log-stream"
-    log_group_name = aws_cloudwatch_log_group.deliberation-empirica-log-group.name
+  name           = "deliberation-empirica-log-stream"
+  log_group_name = aws_cloudwatch_log_group.deliberation-empirica-log-group.name
 
 }
 
 resource "aws_ecs_task_definition" "deliberation-empirica-app" {
-  family                   = "deliberation-empirica-app"
-  container_definitions    = jsonencode([
-  {
-    name      = "deliberation-empirica-container",
-    image     = "ghcr.io/watts-lab/deliberation-empirica:latest", 
-    cpu       = 10,
-    memory    = 512,
-    essential = true,
-    portMappings = [
+  family = "deliberation-empirica-app"
+  container_definitions = jsonencode([
+    {
+      name      = "deliberation-empirica-container",
+      image     = "ghcr.io/watts-lab/deliberation-empirica:latest",
+      cpu       = 10,
+      memory    = 512,
+      essential = true,
+      portMappings = [
         {
-        containerPort = 3000
-        hostPort      = 80
+          containerPort = 3000
+          hostPort      = 80
         }
-    ]
-    mountPoints = [
-      {
-        sourceVolume  = "deliberation-data-volume"
-        containerPath = "/data"
-      }
-    ]
-    logConfiguration = {
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "deliberation-data-volume"
+          containerPath = "/data"
+        }
+      ]
+      logConfiguration = {
         logDriver = "awslogs"
         options = {
-            awslogs-group = aws_cloudwatch_log_group.deliberation-empirica-log-group.name,
-            awslogs-region = "us-east-1",
-            awslogs-stream-prefix = "ecs"
+          awslogs-group         = aws_cloudwatch_log_group.deliberation-empirica-log-group.name,
+          awslogs-region        = "us-east-1",
+          awslogs-stream-prefix = "ecs"
         }
+      }
     }
-  }
   ])
 
   volume {
     name = "deliberation-data-volume"
     efs_volume_configuration {
-      file_system_id          = aws_efs_file_system.efs.id
-      transit_encryption      = "ENABLED"
+      file_system_id     = aws_efs_file_system.efs.id
+      transit_encryption = "ENABLED"
       authorization_config {
-        access_point_id       = aws_efs_access_point.efs.id
-        iam                   = "ENABLED"
+        access_point_id = aws_efs_access_point.efs.id
+        iam             = "ENABLED"
       }
     }
   }
@@ -95,11 +99,10 @@ resource "aws_ecs_service" "deliberation-empirica-app" {
 
 }
 
-
-
 resource "aws_lb_target_group" "example" {
-  name_prefix        = "delib"
-  port               = 80
-  protocol           = "HTTP"
-  target_type        = "ip"
+  name_prefix = "delib"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.deliberation.id
 }
