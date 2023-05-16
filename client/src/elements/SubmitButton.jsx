e03a6e37ec9100cc8d204897dd9a437931520f8f
James Houghton <james.p.houghton@gmail.com>
2023-05-16 13:28:41 -0400

    Bring changed test to mockCDN

--- client/src/elements/SubmitButton.jsx
+++ client/src/elements/SubmitButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "../components/Button";
 
 // buttonText changes based on what the treatment yaml file specifies
-export function SubmitButton({ onSubmit, buttonText }) {
+export function SubmitButton({ onSubmit, buttonText = "Next" }) {
   return (
     <div className="mt-4">
       <Button testId="submitButton" handleClick={onSubmit}>
