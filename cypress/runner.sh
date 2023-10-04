fa04aea52101585a64720e15b991de7a119c268a
James Houghton <james.p.houghton@gmail.com>
2023-10-04 17:32:51 -0400

    Update runner.sh

--- cypress/runner.sh
+++ cypress/runner.sh
@@ -3,7 +3,10 @@
 # starts the mock CDN server and the empirca server ready for testing
 
 npx --yes serve fixtures/mockCDN/ -l 9091 &
- 
+
+cd ../etherpad
+docker compose up
+
 cd ..
 mkdir -p data
 TEST_CONTROLS=enabled DATA_DIR=$(pwd)/data empirica --tajriba.store.file=$(pwd)/data/tajriba.json
