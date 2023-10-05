5e76243324f9fbe2768d1598aaf5014eb563880d
James Houghton <james.p.houghton@gmail.com>
2023-10-05 13:55:14 -0400

    working on registering etherpad container

--- server/src/etherpad.js
+++ server/src/etherpad.js
@@ -18,58 +18,6 @@ export async function createEtherpad({ padId, defaultText }) {
   createPadUrl.searchParams.set("text", defaultText);
 
   const padUrl = new URL(`/p/${padId}`, baseURL.replace("[::1]", "localhost"));
-  // const opts = {
-  //   agent: new http.Agent({
-  //     keepAlive: true,
-  //   }),
-  // };
-
-  // fetch(createPadUrl.toString(), opts).then((res) => console.log(res));
-
-  // const clientURL = `${process.env.ETHERPAD_BASE_URL}/p/${padId}`;
-
-  // if (!defaultText) return clientURL;
-
-  // console.log(`Creating new etherpad at ${padId}`);
-
-  // const data = {
-  //   apikey: process.env.ETHERPAD_API_KEY,
-  //   padID: padId,
-  //   text: encodeURI(defaultText),
-  // };
-
-  // axios
-  //   .post(`${process.env.ETHERPAD_BASE_URL}/api/1/createPad`, data)
-  //   .then((response) => {
-  //     if (response.data.code !== 0) {
-  //       console.log(
-  //         `Status code error creating new etherpad at ${padId} with text: ${defaultText}`,
-  //         response.data
-  //       );
-  //     }
-  //     etherpadList.set(padId, clientURL);
-  //     return clientURL;
-  //   })
-  //   .catch((error) => {
-  //     console.log(
-  //       `Error creating new etherpad at ${padId} with text: ${defaultText}`,
-  //       error
-  //     );
-  //     return undefined;
-  //   });
-
-  // const headers = {
-  //   "Content-Type": "application/x-www-form-urlencoded",
-  // };
-  // const data = {
-  //   text: encodeURI(defaultText),
-  // };
-
-  // const params = new URLSearchParams({
-  //   apikey: process.env.ETHERPAD_API_KEY,
-  //   padID: padId,
-  //   text: defaultText,
-  // })
 
   try {
     const response = await axios.get(createPadUrl.toString());
@@ -85,69 +33,6 @@ export async function createEtherpad({ padId, defaultText }) {
   } catch (error) {
     console.log(`Error creating pad ${padId}`, error);
   }
-
-  // axios
-  //   .get(createPadUrl.toString())
-  //   .then((response) => {
-
-  //   })
-  //   .catch((error) => {
-  //     console.log(`Error creating pad ${padId}`, error);
-  //   });
-
-  // const params = {
-  //   apikey: process.env.ETHERPAD_API_KEY,
-  //   padID: padId,
-  //   text: encodeURI(defaultText),
-  // };
-
-  // axios
-  //   .get(`${process.env.ETHERPAD_BASE_URL}/api/1/createPad`, {
-  //     params,
-  //     responseType: "json",
-  //   })
-  //   .then((response) => {
-  //     console.log("Response", response);
-  //     if (response.data.code !== 0) {
-  //       console.log(
-  //         `Status code error creating new etherpad at ${padId} with text: ${defaultText}`,
-  //         response.data
-  //       );
-  //     }
-  //     etherpadList.set(padId, clientURL);
-  //     return clientURL;
-  //   })
-  //   .catch((error) => {
-  //     console.log(
-  //       `Error creating new etherpad at ${padId} with text: ${defaultText}`,
-  //       error
-  //     );
-  //     return undefined;
-  //   });
-
-  // set pad text
-  // await axios
-  //   .get(`${process.env.ETHERPAD_BASE_URL}/api/1/createPad`, {
-  //     params,
-  //   })
-  //   .then((response) => {
-  //     if (response.data.code !== 0) {
-  //       console.log(
-  //         `Status code error creating new etherpad at ${padId} with text: ${defaultText}`,
-  //         response.data
-  //       );
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     console.log(
-  //       `Error creating new etherpad at ${padId} with text: ${defaultText}`,
-  //       error
-  //     );
-  //     return undefined;
-  //   });
-
-  // etherpadList.set(padId, clientURL);
-  // return clientURL;
 }
 
 export async function getEtherpadText({ padId }) {
