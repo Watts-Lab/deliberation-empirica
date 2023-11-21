6b659acf7cfb8594d01426f0beb70ae4e5fee449
gumillie222 <121700352+gumillie222@users.noreply.github.com>
2023-11-21 13:41:30 -0500

    added left padding on textbox (#667)
    Co-authored-by: James Houghton <james.p.houghton@gmail.com>

--- client/src/components/TextChat.jsx
+++ client/src/components/TextChat.jsx
@@ -197,7 +197,7 @@ function Input({ onNewMessage }) {
         name="message"
         id="message"
         rows={1}
-        className="peer resize-none bg-transparent block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-200 placeholder:text-gray-300 focus:ring-2 focus:ring-inset focus:ring-empirica-500 sm:text-sm sm:leading-6"
+        className="peer resize-none bg-transparent block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-200 placeholder:text-gray-300 focus:ring-2 focus:ring-inset focus:ring-empirica-500 sm:text-sm sm:leading-6"
         placeholder="Say something"
         onKeyDown={handleKeyDown}
         onKeyUp={handleKeyUp}
