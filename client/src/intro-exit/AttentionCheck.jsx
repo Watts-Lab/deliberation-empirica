edb6bb6fde193a4369cd8b17bf1e249e6f461a7a
James Houghton <james.p.houghton@gmail.com>
2023-11-22 11:43:52 -0500

    consolidate mark tags

--- client/src/intro-exit/AttentionCheck.jsx
+++ client/src/intro-exit/AttentionCheck.jsx
@@ -3,119 +3,80 @@ import { Button } from "../components/Button";
 import { H1 } from "../components/TextStyles";
 
 export function AttentionCheck({ next }) {
-
-  const [correctUntil, setCorrectUntil] = useState(0)
+  const [correctUntil, setCorrectUntil] = useState(undefined);
+  const [sentenceInput, setSentenceInput] = useState("");
+  const originalString =
+    "I agree to participate in this study to the best of my ability.";
 
   useEffect(() => {
     console.log("Intro: Attention Check");
   }, []);
 
-  const labelClassName =
-    "mb-5 mt-3 appearance-none block px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-empirica-500 focus:border-empirica-500 w-full";
-
-  const [sentenceInput, setSentenceInput] = useState("");
-  const originalString = "Last Friday I saw a spotted striped blue worm shake hands with a legless lizard."
-
-  const handleAttentionCheck = (e) => {
-    setSentenceInput(e.target.value)
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
     if (originalString !== sentenceInput) {
-      console.log("sentences don't match")
+      console.log("sentences don't match");
       // Find the index where the sentences don't match
       let mismatchIndex = 0;
-      while (mismatchIndex < originalString.length && 
-        sentenceInput[mismatchIndex] === originalString[mismatchIndex]) {
+      while (
+        mismatchIndex < originalString.length &&
+        sentenceInput[mismatchIndex] === originalString[mismatchIndex]
+      ) {
         mismatchIndex += 1;
       }
-
-      setCorrectUntil(mismatchIndex)
-
-      // Create the marked HTML content
-      // const markedSentence = originalString
-      //   .slice(0, mismatchIndex) // Matched part
-      //   .replace(/(.+)$/,'<mark style="background-color:#80f880">$1</mark>') // Mark the last matched character
-      //   + originalString.slice(mismatchIndex) // Rest of the original string
-      //   .replace(/(.+)$/,'<mark style="background-color:#f88080">$1</mark>'); // Mark the remaining characters
-
-
-      // document.getElementById("originalString").innerHTML = markedSentence;
-      // // highlight matched parts with green and unmatched part with red
-      // let matched = "";
-      // let i = 0;
-      // while (i < sentenceInput.length && sentenceInput.split('')[i] === originalString.split('')[i]) {
-      //   matched += sentenceInput.split('')[i];
-      //   i+=1;
-      // }
-      // // console.log(matched);
-      // // render up til the index change coloring
-      // let newS = originalString.replace(matched, `<mark style="background-color:#80f880">${matched}</mark>`);
-      // const rest = originalString.replace(matched, '');
-      // newS = newS.replace(rest, `<mark style="background-color:#f88080">${rest}</mark>`)
-      // document.getElementById("originalString").innerHTML = newS;
+      setCorrectUntil(mismatchIndex);
     } else {
       // continue to the next step if matched exactly
       next();
     }
   };
 
-  const renderMarkedSentence = () => {
-    const markedSentence = [];
-    
-    for (let i = 0; i < originalString.length; i++) {
-      if (i < correctUntil) {
-        markedSentence.push(
-          <mark key={i} style={{ backgroundColor: "#80f880" }}>
-            {originalString[i]}
-          </mark>
-        );
-      } else {
-        markedSentence.push(
-          <mark key={i} style={{ backgroundColor: "#f88080" }}>
-            {originalString[i]}
-          </mark>
-        );
-      }
+  const renderSentence = () => {
+    if (correctUntil === undefined) {
+      return originalString;
     }
-  
     return (
-      <p>{markedSentence}</p>
+      <>
+        <mark data-test="correctPortion" style={{ backgroundColor: "#80f880" }}>
+          {originalString.substring(0, correctUntil)}
+        </mark>
+        <mark
+          data-test="incorrectPortion"
+          style={{ backgroundColor: "#f88080" }}
+        >
+          {originalString.substring(correctUntil)}
+        </mark>
+      </>
     );
   };
-
-  
-  
-  
 
   return (
     <div className="grid justify-center">
       <div className="max-w-xl">
-        <H1>
-          In the box below, please copy the following sentence exactly:
-        </H1>
-        <span id="originalString"
-            onCopy={(e) => {
-              e.preventDefault();
-              return false;
-            }}
-            onDrag={(e) => {
-              e.preventDefault();
-              return false;
-            }}>
-          {renderMarkedSentence()}
+        <H1>Please type the following sentence exactly:</H1>
+        <span
+          id="originalString"
+          onCopy={(e) => {
+            e.preventDefault();
+            return false;
+          }}
+          onDrag={(e) => {
+            e.preventDefault();
+            return false;
+          }}
+        >
+          <p className="my-4 pl-3 bg-gray-100">{renderSentence()}</p>
         </span>
-        
+
         <form onSubmit={handleSubmit}>
           <input
-            className={labelClassName}
+            className="mb-5 mt-3 appearance-none block px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-empirica-500 focus:border-empirica-500 w-full"
             type="textarea"
             autoComplete="off"
             id="inputAttentionCheck"
             data-test="inputAttentionCheck"
-            onChange={handleAttentionCheck}
-            onPaste= {(e) => {
+            onChange={(e) => setSentenceInput(e.target.value)}
+            onPaste={(e) => {
               e.preventDefault();
               return false;
             }}
@@ -129,6 +90,11 @@ export function AttentionCheck({ next }) {
             }}
             // autoFocus
           />
+          {correctUntil !== undefined && (
+            <p className="text-red-600 text-sm italic mb-5">
+              Please correct any errors
+            </p>
+          )}
           <Button testId="continueAttentionCheck" handleClick={handleSubmit}>
             <p>Continue</p>
           </Button>
--- cypress/support/sharedSteps.js
+++ cypress/support/sharedSteps.js
@@ -206,61 +206,71 @@ Cypress.Commands.add("stepNickname", (playerKey) => {
 Cypress.Commands.add("stepAttentionCheck", (playerKey) => {
   cy.log(`⌛️ Intro: Attention Check ${playerKey}`);
 
-  cy.get(`[test-player-id="${playerKey}"]`).contains("copy the following sentence", {
-    timeout: 5000,
-  });
+  cy.get(`[test-player-id="${playerKey}"]`).contains(
+    "type the following sentence",
+    { timeout: 5000 }
+  );
 
   // when nothing is entered and submitted, the whole sentence should be red
   cy.get(
     `[test-player-id="${playerKey}"] button[data-test="continueAttentionCheck"]`
   ).click();
 
-  cy.get(`[test-player-id="${playerKey}"] span#originalString`)
-   .find('mark')
-   .should('have.css', 'background-color', 'rgb(248, 128, 128)');
+  cy.get(`[test-player-id="${playerKey}"] mark[data-test="incorrectPortion"]`)
+    .should("have.css", "background-color", "rgb(248, 128, 128)")
+    .contains(
+      "I agree to participate in this study to the best of my ability."
+    );
 
-  
   // check if it allows copy-paste
-  cy.get(`[test-player-id="${playerKey}"] span#originalString`)
-  .trigger("keydown", { keyCode: 67, ctrlKey: true });
+  cy.get(`[test-player-id="${playerKey}"] span#originalString`).trigger(
+    "keydown",
+    { keyCode: 67, ctrlKey: true }
+  );
 
-  cy.get(`[test-player-id="${playerKey}"] input[data-test="inputAttentionCheck"]`)
-  .focus().trigger("keydown", { keyCode: 86, ctrlKey: true });
+  cy.get(
+    `[test-player-id="${playerKey}"] input[data-test="inputAttentionCheck"]`
+  )
+    .focus()
+    .trigger("keydown", { keyCode: 86, ctrlKey: true });
 
-  cy.get(`[test-player-id="${playerKey}"] input[data-test="inputAttentionCheck"]`)
-  .should(($input) => {
-    expect($input.val()).to.equal('');
+  cy.get(
+    `[test-player-id="${playerKey}"] input[data-test="inputAttentionCheck"]`
+  ).should(($input) => {
+    expect($input.val()).to.equal("");
   });
 
-  // check if it allows drag and drop
+  // TODO: check if it allows drag and drop
 
   // when only parts are entered and submitted, it should be half green half red
   cy.get(
     `[test-player-id="${playerKey}"] input[data-test="inputAttentionCheck"]`
-  ).type(`Last Friday`, { force: true });
+  ).type(`I agree to eat a starfish`, { force: true });
 
   cy.get(
     `[test-player-id="${playerKey}"] button[data-test="continueAttentionCheck"]`
   ).click();
 
-  cy.get(`[test-player-id="${playerKey}"] span#originalString`)
-  .find('mark').eq(2)
-  .should('have.css', 'background-color', 'rgb(128, 248, 128)');
+  cy.get(`[test-player-id="${playerKey}"] mark[data-test="correctPortion"]`)
+    .should("have.css", "background-color", "rgb(128, 248, 128)")
+    .contains("I agree to ");
 
-  cy.get(`[test-player-id="${playerKey}"] span#originalString`)
-  .find('mark').eq(30)
-  .should('have.css', 'background-color', 'rgb(248, 128, 128)');
+  cy.get(`[test-player-id="${playerKey}"] mark[data-test="incorrectPortion"]`)
+    .should("have.css", "background-color", "rgb(248, 128, 128)")
+    .contains("participate in this study to the best of my ability.");
 
   // when the correct sentence is entered, pass the stage
   cy.get(
     `[test-player-id="${playerKey}"] input[data-test="inputAttentionCheck"]`
-  ).type(` I saw a spotted striped blue worm shake hands with a legless lizard.`, { force: true });
+  ).type(
+    `{selectall}{backspace}I agree to participate in this study to the best of my ability.`,
+    { force: true }
+  );
 
   cy.get(
     `[test-player-id="${playerKey}"] button[data-test="continueAttentionCheck"]`
   ).click();
-
-})
+});
 
 Cypress.Commands.add("stepCountdown", (playerKey) => {
   cy.log(`⌛️ Wait: countdown`);
