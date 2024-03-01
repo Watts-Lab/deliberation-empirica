7360e4e8e17e086e4157741684c7eebf760c41f1
James Houghton <james.p.houghton@gmail.com>
2024-03-01 14:03:15 -0500

    Update dispatch.js

--- server/src/preFlight/dispatch.js
+++ server/src/preFlight/dispatch.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-syntax */
-import { info } from "@empirica/core/console";
+import { info, warn } from "@empirica/core/console";
 import { compare } from "../utils/comparison";
 
 function shuffle(arr) {
@@ -57,23 +57,183 @@ function getMinCost(treatments, nParticipants, noAssignCost) {
   return minCost;
 }
 
+function getTheoreticalMaxPayoff(
+  treatments,
+  payoffs,
+  knockdowns,
+  nParticipants
+) {
+  // The theoretical maximum payoff is the sum of the maximum payoffs for each treatment
+}
+
 export function makeDispatcher({
   treatments,
-  noAssignCost = 50,
-  requiredFractionOfMaximumImprovement = 0.9,
+  payoffs: payoffsArg,
+  knockdowns,
+  requiredFractionOfMaximumPayoff = 0.9,
   maxIter = 3000,
   minIter = 100,
 }) {
+  // Arguments:
+  // - treatments: an array of length n of treatment objects taken directly from the treatment file.
+  //   Needs to be in the order that the treatment names are specified in the batch config.
+  //
+  // - payoffs: an array of initial payoffs for each treatment taken from the batch config.
+  //
+  // - knockdowns: (optional) how much to reduce the payoff for each treatment when another treatment is used. Must be in the range (0, 1].
+  //   This can take several forms:
+  //   - a single number, in which case the payoff for a treatment is multiplied by this number it is used.
+  //   - an array of numbers of length n, in which case the payoff for a treatment is multiplied by the
+  //     number at its index in the array once it has been used.
+  //   - a matrix of factors by which all treatment payoffs are multiplied when a particular treatment is used.
+  //     Rows (first index) are treatments just used, and columns (second index) are the knockdowns for all treatments given the row.
+  //   - if undefined, we assume that subsequent payoffs are not affected by the use of any treatments.
+  //
+  // - requiredFractionOfMaximumPayoff: the fraction of the theoretically maximum possible payoff in order to stop the search before maxIter.
+  //
+  // - maxIter: the maximum number of iterations to run before stopping the search.
+  // - minIter: the minimum number of iterations to run before stopping the search.
+  //
+  // Returns:
+  // - dispatch: a function that will be called every time the dispatch timer expires, and that will assign participants to treatments.
+
   // Each batch has a single dispatch function that runs multiple times to assign participants to treatments as they arrive
   // we use a closure pattern so that we can persist some state between calls
 
-  // todo:
-  // - persist the isEligibleCache. This will require rewriting the arguments to accept the participant ID instead of index.
-  // - persist the treatmentCosts for the best solution found so far, so that we can use it as a starting point for the next call.
-  //   either that or persist  the count of the number of times each treatment has been used, so that we can calculate the cost of a new solution without having to re-run the whole thing.
-  // - take the treatment costs as an argument
+  if (treatments.length === 0) {
+    throw new Error("No treatments specified");
+  }
 
-  function dispatch(availableParticipants) {
+  let persistentPayoffs;
+  if (!payoffsArg || payoffsArg.length === 0) {
+    warn("No payoffs specified, using default payoffs of 1 for all treatments");
+    persistentPayoffs = treatments.map(() => 1);
+  } else {
+    persistentPayoffs = payoffsArg;
+  }
+
+  if (treatments.length !== payoffsArg.length) {
+    throw new Error(
+      "Number of treatments and payoffs must match, received: ",
+      treatments,
+      payoffsArg
+    );
+  }
+
+  // check that knockdowns are properly formatted and save the type for later use
+  let knockdownType;
+  if (!knockdowns) {
+    knockdownType = "none";
+  } else if (
+    typeof knockdowns === "number" &&
+    knockdowns > 0 &&
+    knockdowns <= 1
+  ) {
+    knockdownType = "single";
+  } else if (
+    Array.isArray(knockdowns) &&
+    knockdowns.length === payoffsArg.length &&
+    knockdowns.every((f) => typeof f === "number" && f > 0 && f <= 1)
+  ) {
+    knockdownType = "array";
+  } else if (
+    Array.isArray(knockdowns) &&
+    knockdowns.length === payoffsArg.length &&
+    knockdowns.every(
+      (f) =>
+        Array.isArray(f) &&
+        f.length === payoffsArg.length &&
+        f.every((ff) => typeof ff === "number" && ff > 0 && ff <= 1)
+    )
+  ) {
+    knockdownType = "matrix";
+  } else {
+    throw new Error("Invalid knockdown factors, received: ", knockdowns);
+  }
+
+  // var persistentTreatmentCosts = treatments.map((treatment) => treatment.cost); // e.g [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+
+  const isEligibleCache = new Map();
+  function isEligible(players, playerId, treatmentIndex, position) {
+    const cacheKey = `${playerId}-${treatmentIndex}-${position}`;
+    if (isEligibleCache.has(cacheKey)) {
+      return isEligibleCache.get(cacheKey);
+    }
+
+    const treatment = treatments[treatmentIndex];
+    const candidate = players.filter((p) => p.id === playerId)[0];
+    const conditions = treatment.groupComposition?.[position] || [];
+
+    for (const condition of conditions) {
+      if (
+        !compare(
+          candidate[condition.promptName],
+          condition.comparator,
+          condition.value
+        )
+      ) {
+        isEligibleCache.set(cacheKey, false);
+        return false;
+      }
+    }
+
+    isEligibleCache.set(cacheKey, true);
+    return true;
+  }
+
+  function knockdown(currentPayoffs, index) {
+    // Apply knockdowns to payoffs
+    // prevPayoffs: the previous array of payoffs for each treatment
+    // index: the index the knockdowns to apply
+    // returns: the new array of payoffs
+    //
+    // Use this after we select a treatment to compute the new payoffs for all treatments
+    // given that this treatment has been selected.
+
+    // no knockdowns
+    if (knockdownType === "none") {
+      return currentPayoffs;
+    }
+
+    // single value for knockdown_factors
+    // only knock down the payoff for the treatment used
+    if (knockdownType === "single") {
+      return currentPayoffs.map((p, i) => (i === index ? p * knockdowns : p));
+    }
+
+    // array of values for knockdown_factors of length n
+    // only knock down the payoff for the treatment used
+    if (knockdownType === "array") {
+      return currentPayoffs.map((p, i) =>
+        i === index ? p * knockdowns[index] : p
+      );
+    }
+
+    // matrix of values for knockdown_factors of size n x n
+    // knock down the payoff for all treatments when a particular treatment is used
+    if (knockdownType === "matrix") {
+      return currentPayoffs.map((p, i) => p * knockdowns[index][i]);
+    }
+
+    throw new Error("Invalid knockdown type");
+  }
+
+  function dispatch(availablePlayers) {
+    // Dispatch participants to treatments
+    //
+    // Arguments:
+    // - availablePlayers: a list of player objects, each containing a playerId and other properties.
+    //   Only players who have not been assigned to a treatment are included.
+    //
+    // Returns an object with the following properties:
+    // - assignments: list of objects, each representing a game to create, and containing properties:
+    //   - treatment: the treatment object for the game
+    //   - list of objects, each representing the assignemnt of a participant to a position in the game, containing properties:
+    //     - participantId: the playerId of the participant
+    //     - position: the position in the game
+    // - cost: the total cost of the assignment
+    // - iterations: the number of iterations required to find the solution
+    //
     // "dispatch" is essentially a depth-first search, where the search tree is assignments of participants to treatment positions
     // We use a recursive function to explore the tree, and we keep track of the best solution found so far at the top level.
     //
@@ -85,82 +245,59 @@ export function makeDispatcher({
     // - Stop if we are within a certain fraction of the minimum possible cost, and we have explored the search space for a minimum number of iterations.
     //
 
-    const isEligibleCache = new Map();
-    function isEligible(participantIndex, treatmentIndex, position) {
-      const cacheKey = `${participantIndex}-${treatmentIndex}-${position}`;
-      if (isEligibleCache.has(cacheKey)) {
-        return isEligibleCache.get(cacheKey);
-      }
-
-      const treatment = treatments[treatmentIndex];
-      const candidate = availableParticipants[participantIndex];
-      const conditions = treatment.groupComposition?.[position] || [];
-
-      for (const condition of conditions) {
-        if (
-          !compare(
-            candidate[condition.promptName],
-            condition.comparator,
-            condition.value
-          )
-        ) {
-          isEligibleCache.set(cacheKey, false);
-          return false;
-        }
-      }
-
-      isEligibleCache.set(cacheKey, true);
-      return true;
-    }
-
-    const nParticipants = availableParticipants.length;
-
-    const startingTreatmentCosts = treatments.map(
-      (treatment) => treatment.cost
-    ); // e.g [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
-    const maxCost = nParticipants * noAssignCost;
-    const minCost = getMinCost(treatments, nParticipants, noAssignCost);
-    const stoppingThreshold =
-      minCost +
-      (maxCost - minCost) * (1 - requiredFractionOfMaximumImprovement);
-
+    const nPlayersAvailable = availablePlayers.length;
     // randomize the order in which we assign participants to remove bias due to arrival time
-    const participantIndices = shuffle(
-      Array.from({ length: nParticipants }, (_, i) => i + 1)
+    const playerIds = shuffle(availablePlayers.map((p) => p.id));
+
+    const maxPayoff = getTheoreticalMaxPayoff(
+      treatments,
+      persistentPayoffs,
+      knockdowns,
+      nPlayersAvailable
     );
 
-    info(`Dispatch with min 'cost' ${minCost}, max 'cost' ${maxCost}`);
+    const stoppingThreshold = maxPayoff * requiredFractionOfMaximumPayoff;
+
+    info(`Dispatch with ${nPlayersAvailable} players`);
 
     let iterCount = 0;
     let stop = false;
-    let currentBestCost = maxCost;
+    let currentBestPayoff = 0;
     let currentBestAssignment = [];
+    let currentBestUpdatedPayoffs = persistentPayoffs;
 
     function recurse({
-      availableParticipantIndices, // list of indices of unassigned participants e.g. [0, 2, 3, 5, 8, 9]
+      unassignedPlayerIds, // list of player ids for players who have not been assigned to a treatment
       committedSlots, // list of tuples of treatment index and position e.g. [[0, 0], [0, 1], [1, 0], [1, 1], [2, 0], [2, 1]]
-      treatmentCosts, // list of costs for each treatment e.g. [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]. When a treatment is used, we increase the cost of the treatment by 1 for the next use.
+      payoffs, // list of payoffs for each treatment given the current state of the search
       partialSolution, // list of tuples of participant index, treatment index, and position e.g. [[0, 0, 0], [2, 0, 1], [3, 1, 0], [5, 1, 1], [8, 2, 0], [9, 2, 1]]
-      partialSolutionCost, // numeric cost of the partial solution e.g. 10
+      partialSolutionPayoff, // payoff of the partial solution e.g. 10
     }) {
       // if this branch can never lead to a solution, abandon it
-      if (committedSlots.length > availableParticipantIndices.length) return;
+      if (committedSlots.length > unassignedPlayerIds.length) return;
 
       // if this branch can never lead to a better solution than the current best, abandon it
-      if (partialSolutionCost >= currentBestCost) return;
+      // if (partialSolutionPayoff >= currentBestCost) return;
+      if (
+        partialSolutionPayoff +
+          Math.max(...payoffs) * unassignedPlayerIds.length <
+        currentBestPayoff
+      )
+        return; // not quite as clean as with the cost formulation
 
       // We have found a solution.
-      if (availableParticipantIndices.length === 0) {
+      if (unassignedPlayerIds.length === 0) {
         // See if it's better than the current best.
-        if (partialSolutionCost < currentBestCost) {
-          currentBestCost = partialSolutionCost;
+        if (partialSolutionPayoff > currentBestPayoff) {
+          currentBestPayoff = partialSolutionPayoff;
           currentBestAssignment = partialSolution;
+          currentBestUpdatedPayoffs = payoffs;
         }
 
         // Is the current best solution good enough to stop searching?
         if (
-          currentBestCost === minCost || // we cannot do better than the minimum cost
-          (currentBestCost < stoppingThreshold && iterCount >= minIter) // we have found an adequate solution after at least minimum exploration
+          currentBestPayoff === maxPayoff || // we cannot do better than the maximum possible payoff, so quit regardless of the number of iterations
+          (currentBestPayoff > stoppingThreshold && iterCount >= minIter) // we have found an adequate solution after at least minimum exploration
         ) {
           stop = true;
         }
@@ -183,19 +320,21 @@ export function makeDispatcher({
       if (committedSlots.length > 0) {
         const [treatmentIndex, position] = committedSlots[0];
 
-        for (const participantIndex of availableParticipantIndices) {
-          if (isEligible(participantIndex, treatmentIndex, position)) {
+        for (const playerId of unassignedPlayerIds) {
+          if (
+            isEligible(availablePlayers, playerId, treatmentIndex, position)
+          ) {
             recurse({
-              availableParticipantIndices: availableParticipantIndices.filter(
-                (i) => i !== participantIndex
+              unassignedPlayerIds: unassignedPlayerIds.filter(
+                (i) => i !== playerId
               ),
               committedSlots: committedSlots.slice(1),
-              treatmentCosts, // no change to treatmentCosts when we fill in already committed slots. We only change treatmentCosts when we commit to a new treatment.
+              payoffs, // no change to payoffs when we fill in already committed slots. We only knock down payoffs when we commit to a new treatment.
               partialSolution: [
                 ...partialSolution,
-                [participantIndex, treatmentIndex, position],
+                [playerId, treatmentIndex, position],
               ],
-              partialSolutionCost, // account for committed slot costs when we make the commitment (below).
+              partialSolutionPayoff, // account for committed slot costs when we make the commitment (below).
             });
           }
 
@@ -212,17 +351,17 @@ export function makeDispatcher({
 
       // store the longest valid partial solution in case we don't ever complete a full solution,
       // we still have something to return if we exceed maxIter
-      if (partialSolutionCost.length > currentBestAssignment.length) {
+      if (partialSolution.length > currentBestAssignment.length) {
         currentBestAssignment = partialSolution;
       }
 
       // work with the first unassigned participant. If we can't assign them here, we'll never be able to.
-      const participantIndex = availableParticipantIndices[0];
+      const playerId = unassignedPlayerIds[0];
 
-      // sort treatments by lowest cost, adding a random decimal to break ties, and return the treatment indices
-      const sortedTreatmentIndices = treatmentCosts
-        .map((cost, index) => [cost + Math.random() / 1000, index])
-        .sort((a, b) => a[0] - b[0])
+      // sort treatments by highest payoff, adding a random decimal to break ties, and return the treatment indices
+      const sortedTreatmentIndices = payoffs
+        .map((cost, index) => [cost + Math.random() / 10000, index])
+        .sort((a, b) => b[0] - a[0])
         .map((pair) => pair[1]);
 
       for (const treatmentIndex of sortedTreatmentIndices) {
@@ -231,23 +370,24 @@ export function makeDispatcher({
           (_, i) => i
         );
         for (const position of positions) {
-          if (isEligible(participantIndex, treatmentIndex, position)) {
-            const newTreatmentCosts = [...treatmentCosts];
-            newTreatmentCosts[treatmentIndex] += 1; // increase the cost of the treatment by 1 for the next use, to encourage diversity in treatment assignment
+          if (
+            isEligible(availablePlayers, playerId, treatmentIndex, position)
+          ) {
+            const newPayoffs = knockdown(payoffs, treatmentIndex);
 
             recurse({
-              availableParticipantIndices: availableParticipantIndices.slice(1),
+              unassignedPlayerIds: unassignedPlayerIds.slice(1),
               committedSlots: positions
                 .filter((p) => p !== position)
                 .map((p) => [treatmentIndex, p]),
-              treatmentCosts: newTreatmentCosts,
+              payoffs: newPayoffs,
               partialSolution: [
                 ...partialSolution,
-                [participantIndex, treatmentIndex, position],
+                [playerId, treatmentIndex, position],
               ],
-              partialSolutionCost:
-                partialSolutionCost +
-                newTreatmentCosts[treatmentIndex] * positions.length,
+              partialSolutionPayoff:
+                partialSolutionPayoff +
+                payoffs[treatmentIndex] * positions.length,
             });
           }
 
@@ -264,35 +404,40 @@ export function makeDispatcher({
       // In this case, we return them to the lobby and pay the noAssignCost.
       // --------------------------------------------------------------
 
-      if (noAssignCost + partialSolutionCost < currentBestCost) {
-        recurse({
-          availableParticipantIndices: availableParticipantIndices.slice(1),
-          committedSlots: [],
-          treatmentCosts,
-          partialSolution: [...partialSolution, [participantIndex, null, null]],
-          partialSolutionCost: partialSolutionCost + noAssignCost,
-        });
-      }
-
-      // if we get here, we couldn't find a solution for this branch that was better than the current best
-      // so we abandon it.
+      recurse({
+        unassignedPlayerIds: unassignedPlayerIds.slice(1),
+        committedSlots: [],
+        payoffs,
+        partialSolution: [...partialSolution, [playerId, null, null]], // no assignment
+        partialSolutionPayoff: partialSolutionPayoff + 0, // no reward for not assigning
+      });
     }
 
     recurse({
-      availableParticipantIndices: participantIndices,
+      unassignedPlayerIds: playerIds,
       committedSlots: [],
-      treatmentCosts: startingTreatmentCosts,
+      payoffs: persistentPayoffs,
       partialSolution: [],
-      partialSolutionCost: 0,
+      partialSolutionPayoff: 0,
     });
 
     // check that everything came out correctly
     // check that every participant has been assigned, and that no participant has been assigned twice
     // check that all games are full
 
+    persistentPayoffs = currentBestUpdatedPayoffs;
+
+    const assignments = 
+    for (playerAssignment of currentBestAssignment) {
+      if (playerAssignment[1] === null) {
+        warn("Unassigned player:", playerAssignment[0]);
+      }
+
+    }
+
     return {
       assignment: currentBestAssignment,
-      cost: currentBestCost,
+      payoff: currentBestPayoff,
       iterations: iterCount,
     };
   }
