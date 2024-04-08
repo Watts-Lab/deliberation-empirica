3e592d58b8994233be21daef89a20979e4bcd0e6
gumillie222 <121700352+gumillie222@users.noreply.github.com>
2024-04-08 09:55:29 -0400

    replace "time remaining" w player title

--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { usePlayer } from "@empirica/core/player/classic/react";
 import { Timer } from "./components/Timer";
 
 export function Profile() {
+  const player = usePlayer();
   return (
     <div
-      data-test="profile"
-      className="absolute top-0 left-0 right-0 md:min-w-2xl  p-y-2 text-gray-500 bg-gray-100 rounded-b-md grid grid-cols-3 items-center shadow-sm"
-    >
-      Time Remaining
-      <Timer />
+    data-test="profile"
+    className="absolute top-0 left-0 right-0 md:min-w-2xl  p-y-2 text-gray-500 bg-gray-100 rounded-b-md grid grid-cols-3 items-center shadow-sm">
+      <div>
+        {player.get("title")}  
+      </div>
+      <div className="flex items-center justify-center">
+        <Timer />
+    </div>
     </div>
   );
 }
