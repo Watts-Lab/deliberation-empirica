4eeecfd9c257e154dc0be0434405ab4b3e56b6c1
gumillie222 <121700352+gumillie222@users.noreply.github.com>
2024-04-28 00:32:02 -0400

    when no title, show "time remaining"

--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -4,12 +4,13 @@ import { Timer } from "./components/Timer";
 
 export function Profile() {
   const player = usePlayer();
+  const title = player.get("title") || "Time Remaining";
   return (
     <div
     data-test="profile"
     className="absolute top-0 left-0 right-0 md:min-w-2xl  p-y-2 text-gray-500 bg-gray-100 rounded-b-md grid grid-cols-3 items-center shadow-sm">
       <div>
-        {player.get("title")}  
+        {title}  
       </div>
       <div className="flex items-center justify-center">
         <Timer />
