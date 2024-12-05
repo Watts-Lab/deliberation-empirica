495b473d61bfb0b4aea5ae7b62bafdfb9461144d
James Houghton <james.p.houghton@gmail.com>
2024-12-05 08:33:23 -0500

    update nogames

--- client/src/intro-exit/NoGames.jsx
+++ client/src/intro-exit/NoGames.jsx
@@ -1,17 +1,3 @@
-/* 
-NoGames.jsx
-James Houghton
-
-If you arrive here without a player object assigned, 
-it means that this was prior to the player registration screen, 
-and so there are no experiments available for the player to join.
-
-If you arrive here with a player object assigned,
-it happened after the player registration (ie, you've done some work)
-but prior to entering the game. (Ie, there should be no game object at this point)
-
-*/
-
 import { usePlayer } from "@empirica/core/player/classic/react";
 import React from "react";
 import { Markdown } from "../components/Markdown";
@@ -20,13 +6,13 @@ const completeMessage = `
 ## 🎉 Thank you for participating!
 The experiment is now finished.
 Please enter the following code to be compensated for your time:
-> 
-`;
+> `;
 
 const closedMessage = `
 ## 🥱 The experiment is now closed.
 We release studies on a regular basis, and we hope that you will have the opportunity to participate soon.
 `;
+
 const noExperimentsMessage = `
 ## ⏳ There are no studies available at this time.
 We release studies on a regular basis, and we hope that you will have the opportunity to participate soon.
@@ -34,17 +20,30 @@ We release studies on a regular basis, and we hope that you will have the opport
 
 export function NoGames() {
   const player = usePlayer();
-  const exitCodes = player?.get("exitCodes");
 
   let message;
   if (player && player.get("playerComplete") === true) {
+    /*
+    You have completed the game, 
+    and are seeing the exit code after the batch has been terminated.
+    */
     message = completeMessage;
+    const exitCodes = player?.get("exitCodes");
     if (exitCodes !== undefined && exitCodes !== "none") {
       message += exitCodes.complete;
     }
   } else if (player) {
+    /*
+    If a player object exists, but you haven't completed the game,
+    you completed registration but didn't finish before we terminated.
+    Probably you left.
+    */
     message = closedMessage;
   } else {
+    /* 
+    The batch closed before you registered, 
+    i.e. there are no experiments available to join.
+    */
     message = noExperimentsMessage;
   }
 
